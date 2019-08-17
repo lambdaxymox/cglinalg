e63b23e359b96c0d9ab802b2e8ee2d4a075f031d
LambdaXymox <lambda.xymox@gmail.com>
2019-08-17 01:33:18 -0500

    Add multiplication implementation for Matrix3.

--- src/matrix.rs
+++ src/matrix.rs
@@ -494,6 +494,26 @@ impl ops::Mul<Matrix3> for Matrix3 {
     }
 }
 
+impl ops::Mul<Matrix3> for &Matrix3 {
+    type Output = Matrix3;
+
+    fn mul(self, other: Matrix3) -> Matrix3 {
+        let c0r0 = self.c0r0 * other.c0r0 + self.c1r0 * other.c0r1 + self.c2r0 * other.c0r2;
+        let c0r1 = self.c0r1 * other.c0r0 + self.c1r1 * other.c0r1 + self.c2r1 * other.c0r2;
+        let c0r2 = self.c0r2 * other.c0r0 + self.c1r2 * other.c0r1 + self.c2r2 * other.c0r2;
+
+        let c1r0 = self.c0r0 * other.c1r0 + self.c1r0 * other.c1r1 + self.c2r0 * other.c1r2;
+        let c1r1 = self.c0r1 * other.c1r0 + self.c1r1 * other.c1r1 + self.c2r1 * other.c1r2;
+        let c1r2 = self.c0r2 * other.c1r0 + self.c1r2 * other.c1r1 + self.c2r2 * other.c1r2;
+
+        let c2r0 = self.c0r0 * other.c2r0 + self.c1r0 * other.c2r1 + self.c2r0 * other.c2r2;
+        let c2r1 = self.c0r1 * other.c2r0 + self.c1r1 * other.c2r1 + self.c2r1 * other.c2r2;
+        let c2r2 = self.c0r2 * other.c2r0 + self.c1r2 * other.c2r1 + self.c2r2 * other.c2r2;
+
+        Matrix3::new(c0r0, c0r1, c0r2, c1r0, c1r1, c1r2, c2r0, c2r1, c2r2)
+    }
+}
+
 
 #[cfg(test)]
 mod matrix2_tests {
