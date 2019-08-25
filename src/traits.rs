228a1c67e5d3b4e9ddee2095ba2732e53780e7cf
LambdaXymox <lambda.xymox@gmail.com>
2019-08-25 16:50:26 -0500

    Remove Metric trait implementation requirement for DotProduct trait.

--- src/traits.rs
+++ src/traits.rs
@@ -4,33 +4,23 @@ use std::ops;
 pub trait Array {
     type Element: Copy;
 
-    ///
     /// The length of the the underlying array.
-    ///
     fn len() -> usize;
 
-    /// 
     /// Generate a pointer to the underlying array for passing a
     /// matrix or vector to the graphics hardware.
-    ///
     fn as_ptr(&self) -> *const Self::Element; 
 
-    /// 
     /// Generate a mutable pointer to the underlying array for passing a
     /// matrix or vector to the graphics hardware.
-    ///
     fn as_mut_ptr(&mut self) -> *mut Self::Element; 
 }
 
 pub trait Zero where Self: Sized + ops::Add<Self, Output = Self> {
-    ///
     /// Create a zero element.
-    ///
     fn zero() -> Self;
 
-    ///
     /// Test whether an element is equal to the zero element.
-    ///
     fn is_zero(&self) -> bool;
 }
 
@@ -67,35 +57,25 @@ pub trait Metric<V: Sized>: Sized {
 }
 
 pub trait DotProduct where Self: VectorSpace {
-    ///
     /// Compute the dot product of two vectors.
-    ///
     fn dot(self, other: Self) -> f32;
 
-    ///
     /// Compute the norm (length) of a vector.
-    ///
     fn norm(self) -> f32 {
         f32::sqrt(self.dot(self))
     }
 
-    ///
     /// Compute the squared norm (length) of a vector.
-    ///
     fn norm2(self) -> f32 {
         self.dot(self)
     }
 
-    ///
     /// Convert an arbitrary vector into a unit vector.
-    ///
     fn normalize(self) -> Self {
         self / self.norm()
     }
 
-    ///
     /// Compute the projection for a vector onto another vector.
-    ///
     fn project_on(self, onto: Self) -> Self {
         onto * (self.dot(onto) / onto.norm2())
     }
