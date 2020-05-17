2f7401311a4ce789f63d039bb0f893e219a437a6
LambdaXymox <lambda.xymox@gmail.com>
2020-05-17 01:30:14 -0500

    Generate more properties for vector arithmetic.

--- tests/vector.rs
+++ tests/vector.rs
@@ -3,7 +3,7 @@ extern crate num_traits;
 extern crate proptest;
 
 use proptest::prelude::*;
-use cgmath::{Vector1, Zero, Scalar};
+use cgmath::{Vector1, Vector2, Vector3, Vector4, Zero, Scalar};
 
 /// A macro that generates the property tests for vector indexing.
 /// `$VectorN` denotes the name of the vector type.
@@ -54,6 +54,17 @@ fn any_vector1<S>() -> impl Strategy<Value = Vector1<S>> where S: Scalar + Arbit
     any::<S>().prop_map(|x| Vector1::new(x))
 }
 
+fn any_vector2<S>() -> impl Strategy<Value = Vector2<S>> where S: Scalar + Arbitrary {
+    any::<(S, S)>().prop_map(|(x, y)| Vector2::new(x, y))
+}
+
+fn any_vector3<S>() -> impl Strategy<Value = Vector3<S>> where S: Scalar + Arbitrary {
+    any::<(S, S, S)>().prop_map(|(x, y, z)| Vector3::new(x, y, z))
+}
+
+fn any_vector4<S>() -> impl Strategy<Value = Vector4<S>> where S: Scalar + Arbitrary {
+    any::<(S, S, S, S)>().prop_map(|(x, y, z, w)| Vector4::new(x, y, z, w))
+}
 
 macro_rules! vector_arithmetic_props {
     ($VectorN:ident, $FieldType:ty, $Generator:ident, $TestModuleName:ident) => {
@@ -89,3 +100,31 @@ vector_arithmetic_props!(Vector1, u32, any_vector1, vector1_u32_arithmetic_props
 vector_arithmetic_props!(Vector1, u64, any_vector1, vector1_u64_arithmetic_props);
 vector_arithmetic_props!(Vector1, u128, any_vector1, vector1_u128_arithmetic_props);
 vector_arithmetic_props!(Vector1, usize, any_vector1, vector1_usize_arithmetic_props);
+
+vector_arithmetic_props!(Vector2, f32, any_vector2, vector2_f32_arithmetic_props);
+vector_arithmetic_props!(Vector2, f64, any_vector2, vector2_f64_arithmetic_props);
+vector_arithmetic_props!(Vector2, u8, any_vector2, vector2_u8_arithmetic_props);
+vector_arithmetic_props!(Vector2, u16, any_vector2, vector2_u16_arithmetic_props);
+vector_arithmetic_props!(Vector2, u32, any_vector2, vector2_u32_arithmetic_props);
+vector_arithmetic_props!(Vector2, u64, any_vector2, vector2_u64_arithmetic_props);
+vector_arithmetic_props!(Vector2, u128, any_vector2, vector2_u128_arithmetic_props);
+vector_arithmetic_props!(Vector2, usize, any_vector2, vector2_usize_arithmetic_props);
+
+vector_arithmetic_props!(Vector3, f32, any_vector3, vector3_f32_arithmetic_props);
+vector_arithmetic_props!(Vector3, f64, any_vector3, vector3_f64_arithmetic_props);
+vector_arithmetic_props!(Vector3, u8, any_vector3, vector3_u8_arithmetic_props);
+vector_arithmetic_props!(Vector3, u16, any_vector3, vector3_u16_arithmetic_props);
+vector_arithmetic_props!(Vector3, u32, any_vector3, vector3_u32_arithmetic_props);
+vector_arithmetic_props!(Vector3, u64, any_vector3, vector3_u64_arithmetic_props);
+vector_arithmetic_props!(Vector3, u128, any_vector3, vector3_u128_arithmetic_props);
+vector_arithmetic_props!(Vector3, usize, any_vector3, vector3_usize_arithmetic_props);
+
+vector_arithmetic_props!(Vector4, f32, any_vector4, vector4_f32_arithmetic_props);
+vector_arithmetic_props!(Vector4, f64, any_vector4, vector4_f64_arithmetic_props);
+vector_arithmetic_props!(Vector4, u8, any_vector4, vector4_u8_arithmetic_props);
+vector_arithmetic_props!(Vector4, u16, any_vector4, vector4_u16_arithmetic_props);
+vector_arithmetic_props!(Vector4, u32, any_vector4, vector4_u32_arithmetic_props);
+vector_arithmetic_props!(Vector4, u64, any_vector4, vector4_u64_arithmetic_props);
+vector_arithmetic_props!(Vector4, u128, any_vector4, vector4_u128_arithmetic_props);
+vector_arithmetic_props!(Vector4, usize, any_vector4, vector4_usize_arithmetic_props);
+
