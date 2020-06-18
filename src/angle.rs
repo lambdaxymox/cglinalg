4f951345b7d8f69883d46869d1845d59bb7aca5b
LambdaXymox <lambda.xymox@gmail.com>
2020-06-17 20:21:08 -0500

    Implement arithmetic operations for radians.

--- src/angle.rs
+++ src/angle.rs
@@ -346,3 +346,325 @@ impl<S> approx::UlpsEq for Degrees<S> where S: ScalarFloat {
         S::ulps_eq(&self.0, &other.0, epsilon, max_ulps)
     }
 }
+
+
+
+
+impl<S> ops::Add<Radians<S>> for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn add(self, other: Radians<S>) -> Self::Output {
+        Radians(self.0 + other.0)
+    } 
+}
+
+impl<'a, S> ops::Add<&'a Radians<S>> for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn add(self, other: &'a Radians<S>) -> Self::Output {
+        Radians(self.0 + other.0)
+    } 
+}
+
+impl<'a, S> ops::Add<Radians<S>> for &'a Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn add(self, other: Radians<S>) -> Self::Output {
+        Radians(self.0 + other.0)
+    } 
+}
+
+impl<'a, 'b, S> ops::Add<&'a Radians<S>> for &'b Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn add(self, other: &'a Radians<S>) -> Self::Output {
+        Radians(self.0 + other.0)
+    } 
+}
+
+impl<S> ops::Sub<Radians<S>> for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn sub(self, other: Radians<S>) -> Self::Output {
+        Radians(self.0 + other.0)
+    } 
+}
+
+impl<'a, S> ops::Sub<&'a Radians<S>> for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn sub(self, other: &'a Radians<S>) -> Self::Output {
+        Radians(self.0 + other.0)
+    } 
+}
+
+impl<'a, S> ops::Sub<Radians<S>> for &'a Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn sub(self, other: Radians<S>) -> Self::Output {
+        Radians(self.0 + other.0)
+    } 
+}
+
+impl<'a, 'b, S> ops::Sub<&'a Radians<S>> for &'b Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn sub(self, other: &'a Radians<S>) -> Self::Output {
+        Radians(self.0 + other.0)
+    } 
+}
+
+impl<S> ops::Mul<S> for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn mul(self, other: S) -> Self::Output {
+        Radians(self.0 * other)
+    }
+}
+
+impl<'a, S> ops::Mul<S> for &'a Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn mul(self, other: S) -> Self::Output {
+        Radians(self.0 * other)
+    }
+}
+
+impl<S> ops::Mul<Radians<S>> for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn mul(self, other: Radians<S>) -> Self::Output {
+        Radians(self.0 * other.0)
+    }
+}
+
+impl<'a, S> ops::Mul<&'a Radians<S>> for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn mul(self, other: &'a Radians<S>) -> Self::Output {
+        Radians(self.0 * other.0)
+    }
+}
+
+impl<'a, S> ops::Mul<Radians<S>> for &'a Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn mul(self, other: Radians<S>) -> Self::Output {
+        Radians(self.0 * other.0)
+    }
+}
+
+impl<'a, 'b, S> ops::Mul<&'a Radians<S>> for &'b Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn mul(self, other: &'a Radians<S>) -> Self::Output {
+        Radians(self.0 * other.0)
+    }
+}
+
+impl<S> ops::Div<S> for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn div(self, other: S) -> Self::Output {
+        Radians(self.0 / other)
+    }
+}
+
+impl<'a, S> ops::Div<S> for &'a Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn div(self, other: S) -> Self::Output {
+        Radians(self.0 / other)
+    }
+}
+
+impl<S> ops::Div<Radians<S>> for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn div(self, other: Radians<S>) -> Self::Output {
+        Radians(self.0 / other.0)
+    }
+}
+
+impl<'a, S> ops::Div<&'a Radians<S>> for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn div(self, other: &'a Radians<S>) -> Self::Output {
+        Radians(self.0 / other.0)
+    }
+}
+
+impl<'a, S> ops::Div<Radians<S>> for &'a Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn div(self, other: Radians<S>) -> Self::Output {
+        Radians(self.0 / other.0)
+    }
+}
+
+impl<'a, 'b, S> ops::Div<&'a Radians<S>> for &'b Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn div(self, other: &'a Radians<S>) -> Self::Output {
+        Radians(self.0 / other.0)
+    }
+}
+
+impl<S> ops::Rem<Radians<S>> for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn rem(self, other: Radians<S>) -> Self::Output {
+        Radians(self.0 % other.0)
+    }
+}
+
+impl<'a, S> ops::Rem<&'a Radians<S>> for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn rem(self, other: &'a Radians<S>) -> Self::Output {
+        Radians(self.0 % other.0)
+    }
+}
+
+impl<'a, S> ops::Rem<Radians<S>> for &'a Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn rem(self, other: Radians<S>) -> Self::Output {
+        Radians(self.0 % other.0)
+    }
+}
+
+impl<'a, 'b, S> ops::Rem<&'a Radians<S>> for &'b Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn rem(self, other: &'a Radians<S>) -> Self::Output {
+        Radians(self.0 % other.0)
+    }
+}
+
+impl<S> ops::Neg for Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn neg(self) -> Self::Output {
+        Radians(-self.0)
+    }
+}
+
+impl<'a, S> ops::Neg for &'a Radians<S> where S: ScalarFloat {
+    type Output = Radians<S>;
+
+    #[inline]
+    fn neg(self) -> Self::Output {
+        Radians(-self.0)
+    }
+}
+
+impl<S> ops::AddAssign<Radians<S>> for Radians<S> where S: ScalarFloat {
+    #[inline]
+    fn add_assign(&mut self, other: Radians<S>) {
+        *self = *self + other;
+    } 
+}
+
+impl<S> ops::SubAssign<Radians<S>> for Radians<S> where S: ScalarFloat {
+    #[inline]
+    fn sub_assign(&mut self, other: Radians<S>) {
+        *self = *self - other;
+    } 
+}
+
+impl<S> ops::MulAssign<S> for Radians<S> where S: ScalarFloat {
+    #[inline]
+    fn mul_assign(&mut self, other: S) {
+        *self = *self * other;
+    } 
+}
+
+impl<S> ops::DivAssign<S> for Radians<S> where S: ScalarFloat {
+    #[inline]
+    fn div_assign(&mut self, other: S) {
+        *self = *self / other;
+    } 
+}
+
+impl<S> ops::RemAssign<Radians<S>> for Radians<S> where S: ScalarFloat {
+    #[inline]
+    fn rem_assign(&mut self, other: Radians<S>) {
+        *self = *self % other;
+    } 
+}
+
+impl<S> num_traits::Zero for Radians<S> where S: ScalarFloat {
+    #[inline]
+    fn zero() -> Radians<S> {
+        Radians(S::zero())
+    }
+
+    #[inline]
+    fn is_zero(&self) -> bool {
+        self.0.is_zero()
+    }
+}
+
+impl<S> approx::AbsDiffEq for Radians<S> where S: ScalarFloat {
+    type Epsilon = <S as approx::AbsDiffEq>::Epsilon;
+
+    #[inline]
+    fn default_epsilon() -> Self::Epsilon {
+        S::default_epsilon()
+    }
+
+    #[inline]
+    fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        S::abs_diff_eq(&self.0, &other.0, epsilon)
+    }
+}
+
+impl<S> approx::RelativeEq for Radians<S> where S: ScalarFloat {
+    #[inline]
+    fn default_max_relative() -> S::Epsilon {
+        S::default_max_relative()
+    }
+
+    #[inline]
+    fn relative_eq(&self, other: &Self, epsilon: S::Epsilon, max_relative: S::Epsilon) -> bool {
+        S::relative_eq(&self.0, &other.0, epsilon, max_relative)
+    }
+}
+
+impl<S> approx::UlpsEq for Radians<S> where S: ScalarFloat {
+    #[inline]
+    fn default_max_ulps() -> u32 {
+        S::default_max_ulps()
+    }
+
+    #[inline]
+    fn ulps_eq(&self, other: &Self, epsilon: S::Epsilon, max_ulps: u32) -> bool {
+        S::ulps_eq(&self.0, &other.0, epsilon, max_ulps)
+    }
+}
