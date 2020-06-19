80929d2350e4ed58acb1ce1899451f3764e4de62
LambdaXymox <lambda.xymox@gmail.com>
2020-06-19 12:31:03 -0500

    Update dependencies.

--- src/cgmath.rs
+++ src/cgmath.rs
@@ -103,16 +103,27 @@ mod tests {
 
     #[test]
     fn test_mat2() {
-        let expected = Matrix2::new(1f32, 2f32, 3f32, 4f32);
-        let result = super::mat2([1f32, 2f32, 3f32, 4f32]);
+        let expected = Matrix2::new(
+            1_f32, 2_f32, 
+            3_f32, 4_f32
+        );
+        let result = super::mat2([
+            1_f32, 2_f32, 3_f32, 4_f32
+        ]);
 
         assert_eq!(result, expected);
     }
 
         #[test]
     fn test_mat3() {
-        let expected = Matrix3::new(1f32, 2f32, 3f32, 4f32, 5f32, 6f32, 7f32, 8f32, 9f32);
-        let result = super::mat3([1f32, 2f32, 3f32, 4f32, 5f32, 6f32, 7f32, 8f32, 9f32]);
+        let expected = Matrix3::new(
+            1_f32, 2_f32, 3_f32, 
+            4_f32, 5_f32, 6_f32, 
+            7_f32, 8_f32, 9_f32
+        );
+        let result = super::mat3([
+            1_f32, 2_f32, 3_f32, 4_f32, 5_f32, 6_f32, 7_f32, 8_f32, 9_f32
+        ]);
 
         assert_eq!(result, expected);
     }
@@ -120,12 +131,12 @@ mod tests {
         #[test]
     fn test_mat4() {
         let expected = Matrix4::new(
-            1f32,  2f32,  3f32,  4f32,  5f32,  6f32,  7f32,  8f32, 
-            9f32, 10f32, 11f32, 12f32, 13f32, 14f32, 15f32, 15f32
+            1_f32,  2_f32,  3_f32,  4_f32,  5_f32,  6_f32,  7_f32,  8_f32, 
+            9_f32, 10_f32, 11_f32, 12_f32, 13_f32, 14_f32, 15_f32, 15_f32
         );
         let result = super::mat4([
-            1f32,  2f32,  3f32,  4f32,  5f32,  6f32,  7f32,  8f32, 
-            9f32, 10f32, 11f32, 12f32, 13f32, 14f32, 15f32, 15f32
+            1_f32,  2_f32,  3_f32,  4_f32,  5_f32,  6_f32,  7_f32,  8_f32, 
+            9_f32, 10_f32, 11_f32, 12_f32, 13_f32, 14_f32, 15_f32, 15_f32
         ]);
 
         assert_eq!(result, expected);
