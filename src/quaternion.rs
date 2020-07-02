71b955fde93e080893dd9e67da2b0a2c52ba53f7
LambdaXymox <lambda.xymox@gmail.com>
2020-07-02 18:25:44 -0500

    Add more slerp unit tests.

--- src/quaternion.rs
+++ src/quaternion.rs
@@ -1117,9 +1117,9 @@ mod slerp_tests {
     }
 
     #[test]
-    fn test_slerp_upper_half_plane() {
-        let angle1 = Degrees(30_f64);
-        let angle2 = Degrees(60_f64);
+    fn test_slerp_upper_right_quadrant1() {
+        let angle1 = Degrees(20_f64);
+        let angle2 = Degrees(70_f64);
         let unit_z = Vector3::unit_z();
         let mut q1 = Quaternion::from_sv(
             Angle::cos(angle1 / 2_f64), 
@@ -1129,7 +1129,30 @@ mod slerp_tests {
             Angle::cos(angle2 / 2_f64), 
             Angle::sin(angle2 / 2_f64) * unit_z
         );
-        let angle_expected = Degrees(45_f64);
+        let angle_expected = Degrees(30_f64);
+        let expected = Quaternion::from_sv(
+            Angle::cos(angle_expected / 2_f64), 
+            Angle::sin(angle_expected / 2_f64) * unit_z
+        );
+        let result = q1.slerp(&q2, 0.2);
+
+        assert!(relative_eq!(result, expected, epsilon = 1e-7));
+    }
+
+    #[test]
+    fn test_slerp_upper_half_plane() {
+        let angle1 = Degrees(30_f64);
+        let angle2 = Degrees(150_f64);
+        let unit_z = Vector3::unit_z();
+        let mut q1 = Quaternion::from_sv(
+            Angle::cos(angle1 / 2_f64), 
+            Angle::sin(angle1 / 2_f64) * unit_z
+        );
+        let q2 = Quaternion::from_sv(
+            Angle::cos(angle2 / 2_f64), 
+            Angle::sin(angle2 / 2_f64) * unit_z
+        );
+        let angle_expected = Degrees(90_f64);
         let expected = Quaternion::from_sv(
             Angle::cos(angle_expected / 2_f64), 
             Angle::sin(angle_expected / 2_f64) * unit_z
