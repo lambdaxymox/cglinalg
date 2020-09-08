2a27a589b135c59718445239856406db1c0a82c4
LambdaXymox <lambda.xymox@gmail.com>
2020-09-08 00:04:13 -0500

    Define points in two dimensions and three dimensions.

--- src/point.rs
+++ src/point.rs
@@ -1,18 +1,19 @@
 use num_traits::NumCast;
 
 
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
 #[repr(C)]
 pub struct Point1<S> {
     pub x: S,
 }
 
 impl<S> Point1<S> {
-    /// Construct a new vector.
+    /// Construct a new point.
     pub const fn new(x: S) -> Point1<S> {
         Point1 { x: x }
     }
 
-    /// Map an operation on the elements of a vector, returning a vector of the 
+    /// Map an operation on the elements of a point, returning a point of the 
     /// new underlying type.
     pub fn map<T, F>(self, mut op: F) -> Point1<T> where F: FnMut(S) -> T {
         Point1 { x: op(self.x) }
@@ -20,7 +21,7 @@ impl<S> Point1<S> {
 }
 
 impl<S> Point1<S> where S: NumCast + Copy {
-    /// Cast a vector from one type of scalars to another type of scalars.
+    /// Cast a point of one type of scalars to a point of another type of scalars.
     pub fn cast<T: NumCast>(&self) -> Option<Point1<T>> {
         let x = match num_traits::cast(self.x) {
             Some(value) => value,
@@ -32,3 +33,89 @@ impl<S> Point1<S> where S: NumCast + Copy {
 }
 
 
+/// A representation of two-dimensional points with a Euclidean metric.
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+#[repr(C)]
+pub struct Point2<S> {
+   pub x: S,
+   pub y: S,
+}
+
+impl<S> Point2<S> {
+    /// Construct a new point.
+    pub const fn new(x: S, y: S) -> Point2<S> {
+        Point2 { x: x, y: y }
+    }
+
+    /// Map an operation on the elements of a point, returning a point of the 
+    /// new underlying type.
+    pub fn map<T, F>(self, mut op: F) -> Point2<T> where F: FnMut(S) -> T {
+        Point2 {
+            x: op(self.x),
+            y: op(self.y),
+        }
+    }
+}
+
+impl<S> Point2<S> where S: NumCast + Copy {
+    /// Cast a point of one type of scalars to a point of another type of scalars.
+    pub fn cast<T: NumCast>(&self) -> Option<Point2<T>> {
+        let x = match num_traits::cast(self.x) {
+            Some(value) => value,
+            None => return None,
+        };
+        let y = match num_traits::cast(self.y) {
+            Some(value) => value,
+            None => return None,
+        };
+
+        Some(Point2::new(x, y))
+    }
+}
+
+
+/// A representation of three-dimensional points in a Euclidean space.
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+#[repr(C)]
+pub struct Point3<S> {
+    pub x: S,
+    pub y: S,
+    pub z: S,
+}
+
+impl<S> Point3<S> {
+    /// Construct a new point.
+    pub const fn new(x: S, y: S, z: S) -> Point3<S> {
+        Point3 { x: x, y: y, z: z }
+    }
+
+    /// Map an operation on the elements of a point, returning a point of the 
+    /// new underlying type.
+    pub fn map<T, F>(self, mut op: F) -> Point3<T> where F: FnMut(S) -> T {
+        Point3 {
+            x: op(self.x),
+            y: op(self.y),
+            z: op(self.z),
+        }
+    }
+}
+
+impl<S> Point3<S> where S: NumCast + Copy {
+    /// Cast a point from one type of scalars to another type of scalars.
+    pub fn cast<T: NumCast>(&self) -> Option<Point3<T>> {
+        let x = match num_traits::cast(self.x) {
+            Some(value) => value,
+            None => return None,
+        };
+        let y = match num_traits::cast(self.y) {
+            Some(value) => value,
+            None => return None,
+        };
+        let z = match num_traits::cast(self.z) {
+            Some(value) => value,
+            None => return None,
+        };
+
+        Some(Point3::new(x, y, z))
+    }
+}
