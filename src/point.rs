ec8c33190f835a6ca01988bcd13ef6f14d88629b
LambdaXymox <lambda.xymox@gmail.com>
2020-09-08 00:35:10 -0500

    Include storage implementations for points.

--- src/point.rs
+++ src/point.rs
@@ -1,4 +1,11 @@
 use num_traits::NumCast;
+use scalar::{
+    Scalar,
+};
+use structure::{
+    Storage,
+};
+use std::mem;
 
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash)]
@@ -31,6 +38,87 @@ impl<S> Point1<S> where S: NumCast + Copy {
         Some(Point1::new(x))
     }
 }
+
+impl<S> Storage for Point1<S> where S: Scalar {
+    type Element = S;
+
+    #[inline]
+    fn len() -> usize {
+        1
+    }
+
+    #[inline]
+    fn shape() -> (usize, usize) {
+        (1, 1)
+    }
+
+    #[inline]
+    fn from_value(value: Self::Element) -> Self {
+        Point1::new(value)
+    }
+
+    #[inline]
+    fn sum(&self) -> Self::Element {
+        self.x
+    }
+
+    #[inline]
+    fn product(&self) -> Self::Element {
+        self.x
+    }
+
+    #[inline]
+    fn as_ptr(&self) -> *const Self::Element {
+        &self.x
+    }
+
+    #[inline]
+    fn as_mut_ptr(&mut self) -> *mut Self::Element {
+        &mut self.x
+    }
+
+    #[inline]
+    fn as_slice(&self) -> &[Self::Element] {
+        <Self as AsRef<[Self::Element; 1]>>::as_ref(self)
+    }
+}
+
+impl<S> AsRef<[S; 1]> for Point1<S> {
+    fn as_ref(&self) -> &[S; 1] {
+        unsafe { mem::transmute(self) }
+    }
+}
+
+impl<S> AsRef<S> for Point1<S> {
+    fn as_ref(&self) -> &S {
+        unsafe { mem::transmute(self) }
+    }
+}
+
+impl<S> AsRef<(S,)> for Point1<S> {
+    fn as_ref(&self) -> &(S,) {
+        unsafe { mem::transmute(self) }
+    }
+}
+
+impl<S> AsMut<[S; 1]> for Point1<S> {
+    fn as_mut(&mut self) -> &mut [S; 1] {
+        unsafe { mem::transmute(self) }
+    }
+}
+
+impl<S> AsMut<S> for Point1<S> {
+    fn as_mut(&mut self) -> &mut S {
+        unsafe { mem::transmute(self) }
+    }
+}
+
+impl<S> AsMut<(S,)> for Point1<S> {
+    fn as_mut(&mut self) -> &mut (S,) {
+        unsafe { mem::transmute(self) }
+    }
+}
+
 
 
 /// A representation of two-dimensional points with a Euclidean metric.
@@ -70,6 +158,74 @@ impl<S> Point2<S> where S: NumCast + Copy {
         };
 
         Some(Point2::new(x, y))
+    }
+}
+
+impl<S> Storage for Point2<S> where S: Scalar {
+    type Element = S;
+
+    #[inline]
+    fn len() -> usize {
+        2
+    }
+
+    #[inline]
+    fn shape() -> (usize, usize) {
+        (2, 1)
+    }
+
+    #[inline]
+    fn from_value(value: Self::Element) -> Self {
+        Point2::new(value, value)
+    }
+
+    #[inline]
+    fn sum(&self) -> Self::Element {
+        self.x + self.y
+    }
+
+    #[inline]
+    fn product(&self) -> Self::Element {
+        self.x * self.y
+    }
+
+    #[inline]
+    fn as_ptr(&self) -> *const Self::Element {
+        &self.x
+    }
+
+    #[inline]
+    fn as_mut_ptr(&mut self) -> *mut Self::Element {
+        &mut self.x
+    }
+
+    #[inline]
+    fn as_slice(&self) -> &[Self::Element] {
+        <Self as AsRef<[Self::Element; 2]>>::as_ref(self)
+    }
+}
+
+impl<S> AsRef<[S; 2]> for Point2<S> {
+    fn as_ref(&self) -> &[S; 2] {
+        unsafe { mem::transmute(self) }
+    }
+}
+
+impl<S> AsRef<(S, S)> for Point2<S> {
+    fn as_ref(&self) -> &(S, S) {
+        unsafe { mem::transmute(self) }
+    }
+}
+
+impl<S> AsMut<[S; 2]> for Point2<S> {
+    fn as_mut(&mut self) -> &mut [S; 2] {
+        unsafe { mem::transmute(self) }
+    }
+}
+
+impl<S> AsMut<(S, S)> for Point2<S> {
+    fn as_mut(&mut self) -> &mut (S, S) {
+        unsafe { mem::transmute(self) }
     }
 }
 
@@ -119,3 +275,73 @@ impl<S> Point3<S> where S: NumCast + Copy {
         Some(Point3::new(x, y, z))
     }
 }
+
+impl<S> Storage for Point3<S> where S: Scalar {
+    type Element = S;
+
+    #[inline]
+    fn len() -> usize {
+        3
+    }
+
+    #[inline]
+    fn shape() -> (usize, usize) {
+        (3, 1)
+    }
+
+    #[inline]
+    fn from_value(value: Self::Element) -> Self {
+        Point3::new(value, value, value)
+    }
+
+    #[inline]
+    fn sum(&self) -> Self::Element {
+        self.x + self.y + self.z
+    }
+
+    #[inline]
+    fn product(&self) -> Self::Element {
+        self.x * self.y * self.z
+    }
+
+    #[inline]
+    fn as_ptr(&self) -> *const Self::Element {
+        &self.x
+    }
+
+    #[inline]
+    fn as_mut_ptr(&mut self) -> *mut Self::Element {
+        &mut self.x
+    }
+
+    #[inline]
+    fn as_slice(&self) -> &[Self::Element] {
+        <Self as AsRef<[Self::Element; 3]>>::as_ref(self)
+    }
+}
+
+
+impl<S> AsRef<[S; 3]> for Point3<S> {
+    fn as_ref(&self) -> &[S; 3] {
+        unsafe { mem::transmute(self) }
+    }
+}
+
+impl<S> AsRef<(S, S, S)> for Point3<S> {
+    fn as_ref(&self) -> &(S, S, S) {
+        unsafe { mem::transmute(self) }
+    }
+}
+
+impl<S> AsMut<[S; 3]> for Point3<S> {
+    fn as_mut(&mut self) -> &mut [S; 3] {
+        unsafe { mem::transmute(self) }
+    }
+}
+
+impl<S> AsMut<(S, S, S)> for Point3<S> {
+    fn as_mut(&mut self) -> &mut (S, S, S) {
+        unsafe { mem::transmute(self) }
+    }
+}
+
