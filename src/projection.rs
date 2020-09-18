4ea9232356d5e84fe136a99f62b2cc544ab72012
LambdaXymox <lambda.xymox@gmail.com>
2020-09-18 16:36:53 -0500

    Write more documentation for the projection module.

--- src/projection.rs
+++ src/projection.rs
@@ -19,15 +19,38 @@ use crate::traits::{
 };
 
 
-/// An orthographic projection with arbitrary left, right, top, bottom,
-/// near, and far planes.
+/// A description of an orthographic projection with arbitrary `left`, `right`, 
+/// `top`, `bottom`, `near`, and `far` planes.
+///
+/// We assume the following constraints to construct a useful orthographic projection
+/// ```text
+/// left   < right
+/// bottom < top
+/// near   < far   (along the negative z-axis).
+/// ```
+/// Each parameter in the specification is a description of the position along an axis
+/// of a plane that the axis is perpendicular to.
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct OrthographicSpec<S> {
+    /// The horizontal position of the left-hand plane in camera space.
+    /// The left-hand plane is a plane parallel to the yz-plane at
+    /// the origin.
     left: S,
+    /// The horizontal position of the right-hand plane in camera space.
+    /// The right-hand plane is a plane parallel to the yz-plane at
+    /// the origin.
     right: S,
+    /// The vertical position of the bottom plane in camera space.
+    /// The bottom plane is a plane parallel to the xz-plane at the origin.
     bottom: S,
+    /// The vertical position of the top plane in camera space.
+    /// the top plane is a plane parallel to the xz plane at the origin.
     top: S,
+    /// The distance along the (-z)-direction of the near plane from the eye.
+    /// The near plane is a plane parallel to the xy-plane at the origin.
     near: S,
+    /// the distance along the (-z)-direction of the far plane from the eye.
+    /// The far plane is a plane parallel to the xy-plane at the origin.
     far: S,
 }
 
@@ -88,15 +111,38 @@ impl<S> Into<OrthographicSpec<S>> for &[S; 6] where S: Copy {
     }
 }
 
-/// A perspective projection based on arbitarary left, right, bottom,
-/// top, near, and far planes.
+/// A perspective projection based on arbitarary `left`, `right`, `bottom`,
+/// `top`, `near`, and `far` planes.
+///
+/// We assume the following constraints to construct a useful perspective projection
+/// ```text
+/// left   < right
+/// bottom < top
+/// near   < far   (along the negative z-axis).
+/// ```
+/// Each parameter in the specification is a description of the position along an axis
+/// of a plane that the axis is perpendicular to.
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct PerspectiveSpec<S> {
+    /// The horizontal position of the left-hand plane in camera space.
+    /// The left-hand plane is a plane parallel to the yz-plane at
+    /// the origin.
     left: S,
+    /// The horizontal position of the right-hand plane in camera space.
+    /// The right-hand plane is a plane parallel to the yz-plane at
+    /// the origin.
     right: S,
+    /// The vertical position of the bottom plane in camera space.
+    /// The bottom plane is a plane parallel to the xz-plane at the origin.
     bottom: S,
+    /// The vertical position of the top plane in camera space.
+    /// the top plane is a plane parallel to the xz plane at the origin.
     top: S,
+    /// The distance along the (-z)-direction of the near plane from the eye.
+    /// The near plane is a plane parallel to the xy-plane at the origin.
     near: S,
+    /// the distance along the (-z)-direction of the far plane from the eye.
+    /// The far plane is a plane parallel to the xy-plane at the origin.
     far: S,
 }
 
@@ -157,17 +203,38 @@ impl<S> Into<PerspectiveSpec<S>> for &[S; 6] where S: Copy {
     }
 }
 
-/// A perspective projection based on the near and farplanes and the vertical
-/// field-of-view angle and the horizontal/vertical aspect ratio.
+/// A perspective projection based on the `near` plane, the `far` plane and the vertical
+/// field of view angle `fovy` and the horizontal/vertical aspect ratio `aspect`.
+///
+/// We assume the following constraints to make a useful perspective projection 
+/// transformation.
+/// ```text
+/// 0 radians < fovy < pi radians
+/// aspect > 0
+/// near < far (along the negative z-axis)
+/// ```
+/// This perspective projection model imposes some constraints on the more general 
+/// perspective specification based on the arbitrary planes. The `fovy` parameter 
+/// combined with the aspect ratio `aspect` ensures that the top and bottom planes 
+/// are the same distance from the eye position along the vertical axis on opposite side. 
+/// They ensure that the `left` and `right` planes are equidistant from the eye on 
+/// opposite sides along the horizontal axis. 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct PerspectiveFovSpec<S> {
+    /// The vertical field of view angle of the perspective transformation viewport.
     fovy: Radians<S>,
+    /// The ratio of the horizontal width to the vertical height.
     aspect: S,
+    /// The position of the near plane along the negative z-axis.
     near: S,
+    /// The position of the far plane along the negative z-axis.
     far: S,
 }
 
 impl<S> PerspectiveFovSpec<S> {
+    /// Construct a new perspective projection operation specification
+    /// based on the vertical field of view angle `fovy`, the `near` plane, the 
+    /// `far` plane, and aspect ratio `aspect`. 
     pub fn new<A: Into<Radians<S>>>(fovy: A, aspect: S, near: S, far: S) -> PerspectiveFovSpec<S> {
         PerspectiveFovSpec {
             fovy: fovy.into(),
@@ -299,9 +366,20 @@ impl<S> From<OrthographicSpec<S>> for Matrix4x4<S> where S: ScalarFloat {
     }
 }
 
+/// A perspective projection tranformation for converting from camera space to
+/// normalized device coordinates.
+///
+/// Orthographic projections differ from perspective projections in that orthographic
+/// projections keeps parallel lines parallel, whereas perspective projections 
+/// preserve the perception of distance. That is, perspective projections preserve 
+/// the spatial ordering in the distance that points lie from the viewing plane. This
+/// property of perspective projection transformations is important for operations
+/// such as z-buffering and occlusion detection.
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct PerspectiveProjection3D<S, Spec> {
+    /// The parameters of the perspective projection.
     spec: Spec,
+    /// The underlying matrix implementing the perspective projection.
     matrix: Matrix4x4<S>,
 }
 
@@ -387,13 +465,23 @@ impl<S, Spec> approx::UlpsEq for PerspectiveProjection3D<S, Spec> where
     }
 }
 
+/// An orthographic projection transformation for converting from camera space to
+/// normalized device coordinates. 
+///
+/// Orthographic projections differ from perspective projections in that orthographic
+/// projections keeps parallel lines parallel, whereas perspective projections 
+/// preserve the perception of distance. That is, perspective projections preserve 
+/// the spatial ordering in the distance that points lie from the viewing plane.
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct OrthographicProjection3D<S> {
+    /// The parameters for the orthographic projection.
     spec: OrthographicSpec<S>,
+    /// The underlying matrix that implements the orthographic projection.
     matrix: Matrix4x4<S>,
 }
 
 impl<S> OrthographicProjection3D<S> where S: ScalarFloat {
+    /// Construct a new orthographic projection.
     pub fn new(spec: OrthographicSpec<S>) -> OrthographicProjection3D<S> {
         OrthographicProjection3D {
             spec: spec,
@@ -401,18 +489,22 @@ impl<S> OrthographicProjection3D<S> where S: ScalarFloat {
         }
     }
 
+    /// Get the parameters defining the orthographic specification.
     pub fn to_spec(&self) -> OrthographicSpec<S> {
         self.spec
     }
 
+    /// Get the underlying matrix implementing the orthographic tranformation.
     pub fn to_matrix(&self) -> &Matrix4x4<S> {
         &self.matrix
     }
 
+    /// Apply the tranformation to a point.
     pub fn transform_point(&self, point: &Point3<S>) -> Point3<S> {
         Point3::from_homogeneous(self.matrix * point.to_homogeneous())
     }
 
+    /// Apply the transformation to a vector.
     pub fn transform_vector(&self, vector: &Vector3<S>) -> Vector3<S> {
         (self.matrix * vector.expand(S::zero())).contract()
     }
