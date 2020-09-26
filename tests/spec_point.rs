363f7c443a9ccf34145a542d27faefc6ae070b5a
LambdaXymox <lambda.xymox@gmail.com>
2020-09-26 01:41:09 -0500

    Add properties to point specification.

--- tests/spec_point.rs
+++ tests/spec_point.rs
@@ -183,3 +183,390 @@ approx_mul_props!(point1_f64_mul_props, Point1, f64, any_point1, any_scalar, 1e-
 approx_mul_props!(point2_f64_mul_props, Point2, f64, any_point2, any_scalar, 1e-7);
 approx_mul_props!(point3_f64_mul_props, Point3, f64, any_point3, any_scalar, 1e-7);
 
+
+/// Generate property tests for point indexing.
+///
+/// ### Macro Parameters
+///
+/// The macro parameters are the following:
+/// * `$TestModuleName` is a name we give to the module we place the tests in 
+///    to separate them from each other for each field type to prevent 
+////   namespace collisions.
+/// * `$PointN` denotes the name of the vector type.
+/// * `$ScalarType` denotes the underlying system of numbers that compose the
+///    set of points.
+/// * `$Generator` is the name of a function or closure for generating examples.
+/// * `$UpperBound` denotes the upperbound on the range of acceptable indices.
+macro_rules! index_props {
+    ($TestModuleName:ident, $Point:ident, $ScalarType:ty, $Generator:ident, $UpperBound:expr) => {
+    #[cfg(test)]
+    mod $TestModuleName {
+        use proptest::prelude::*;
+        use super::{
+            $Generator,
+        };
+
+
+        proptest! {
+            /// When a point is treated like an array, it should accept all indices
+            /// below the length of the array.
+            ///
+            /// Given a point `p`, it should return the entry at position `index` in the 
+            /// underlying storage of the point when the given index is inbounds.
+            #[test]
+            fn prop_accepts_all_indices_in_of_bounds(
+                p in $Generator::<$ScalarType>(), index in 0..$UpperBound as usize) {
+
+                prop_assert_eq!(p[index], p[index]);
+            }
+    
+            /// When a point is treated like an array, it should reject any input index outside
+            /// the length of the array.
+            ///
+            /// Given a point `v`, when the element index `index` is out of bounds, it should 
+            /// generate a panic just like an array indexed out of bounds.
+            #[test]
+            #[should_panic]
+            fn prop_panics_when_index_out_of_bounds(
+                p in $Generator::<$ScalarType>(), index in $UpperBound..usize::MAX) {
+                
+                prop_assert_eq!(p[index], p[index]);
+            }
+        }
+    }
+    }
+}
+
+index_props!(vector1_f64_index_props, Vector1, f64, any_point1, 1);
+index_props!(vector2_f64_index_props, Vector2, f64, any_point2, 2);
+index_props!(vector3_f64_index_props, Vector3, f64, any_point3, 3);
+
+
+/// Generate property tests for point/vector arithmetic over floating point scalars.
+///
+/// ### Macro Parameters
+///
+/// The macro parameters are the following:
+/// * `$TestModuleName` is a name we give to the module we place the property tests
+///    in to separate them from each other for each scalar type to prevent 
+///    namespace collisions.
+/// * `PointN` denotes the name of the point type.
+/// * `$VectorN` denotes the name of the vector type.
+/// * `$ScalarType` denotes the underlying system of numbers that compose the 
+///    set of vectors.
+/// * `$Generator` is the name of a function or closure for generating examples.
+macro_rules! approx_arithmetic_props {
+    ($TestModuleName:ident, $PointN:ident, $VectorN:ident, $ScalarType:ty, $PointGen:ident, $VectorGen:ident, $tolerance:expr) => {
+    #[cfg(test)]
+    mod $TestModuleName {
+        use proptest::prelude::*;
+        use cglinalg::{
+            $VectorN,
+            Zero
+        };
+        use cglinalg::approx::{
+            relative_eq,
+        };
+        use super::{
+            $PointGen,
+            $VectorGen,
+        };
+    
+
+        proptest! {
+            /// A point plus a zero vector equals the same point.
+            ///
+            /// Given a point `p` and a vector `0`
+            /// ```text
+            /// p + 0 = p
+            /// ```
+            #[test]
+            fn prop_point_plus_zero_equals_vector(p in $PointGen()) {
+                let zero_vec = $VectorN::<$ScalarType>::zero();
+
+                prop_assert_eq!(p + zero_vec, p);
+            }
+
+            /// Given a point `p` and a vector `v`, we should be able to use `p` and 
+            /// `v` interchangeably with their references `&p` and `&v` in 
+            /// arithmetic expressions involving points and vectors.
+            ///
+            /// Given a point `p` and a vector `v`, and their references `&p` and 
+            /// `&v`, they should satisfy
+            /// ```text
+            ///  p +  v = &p +  v
+            ///  p +  v =  p + &v
+            ///  p +  v = &p + &v
+            ///  p + &v = &p +  v
+            /// &p +  v =  p + &v
+            /// &p +  v = &p + &v
+            ///  p + &v = &p + &v
+            /// ```
+            #[test]
+            fn prop_point_plus_vector_equals_refpoint_plus_refvector(
+                p in $PointGen::<$ScalarType>(), v in $VectorGen::<$ScalarType>()) {
+                
+                prop_assert_eq!( p +  v, &p +  v);
+                prop_assert_eq!( p +  v,  p + &v);
+                prop_assert_eq!( p +  v, &p + &v);
+                prop_assert_eq!( p + &v, &p +  v);
+                prop_assert_eq!(&p +  v,  p + &v);
+                prop_assert_eq!(&p +  v, &p + &v);
+                prop_assert_eq!( p + &v, &p + &v);
+            }
+
+
+            /// Point and vector addition should be approximately compatible. 
+            /// 
+            /// Given a point `p` and vectors `v1` and `v2`, we have
+            /// ```text
+            /// (p + v1) + v2 ~= p + (v1 + v2)
+            /// ```
+            /// Note that floating point vector addition cannot be exactly 
+            /// associative because arithmetic with floating point numbers 
+            /// is not associative.
+            #[test]
+            fn prop_point_vector_addition_compatible(
+                p in $PointGen::<$ScalarType>(), 
+                v1 in $VectorGen::<$ScalarType>(), v2 in $VectorGen::<$ScalarType>()) {
+
+                prop_assert!(relative_eq!((p + v1) + v2, p + (v1 + v2), epsilon = $tolerance));
+            }
+
+            /// A point minus a zero vector equals the same point.
+            ///
+            /// Given a point `p` and a vector `0`
+            /// ```text
+            /// p - 0 = p
+            /// ```
+            #[test]
+            fn prop_point_minus_zero_equals_vector(p in $PointGen()) {
+                let zero_vec = $VectorN::<$ScalarType>::zero();
+
+                prop_assert_eq!(p - zero_vec, p);
+            }
+
+            /// A point minus itself equals the zero vector.
+            ///
+            /// Given a point `p` and a vector `0`
+            /// ```text
+            /// p - p = 0
+            /// ```
+            #[test]
+            fn prop_point_minus_point_equals_zero_vector(p in $PointGen()) {
+                let zero_vec = $VectorN::<$ScalarType>::zero();
+
+                prop_assert_eq!(p - p, zero_vec);
+            }
+
+            /// Given a point `p` and a vector `v`, we should be able to use `p` and 
+            /// `v` interchangeably with their references `&p` and `&v` in 
+            /// arithmetic expressions involving points and vectors.
+            ///
+            /// Given a point `p` and a vector `v`, and their references `&p` and 
+            /// `&v`, they should satisfy
+            /// ```text
+            ///  p -  v = &p -  v
+            ///  p -  v =  p - &v
+            ///  p -  v = &p - &v
+            ///  p - &v = &p -  v
+            /// &p -  v =  p - &v
+            /// &p -  v = &p - &v
+            ///  p - &v = &p - &v
+            /// ```
+            #[test]
+            fn prop_point_minus_vector_equals_refpoint_plus_refvector(
+                p in $PointGen::<$ScalarType>(), v in $VectorGen::<$ScalarType>()) {
+                
+                prop_assert_eq!( p -  v, &p -  v);
+                prop_assert_eq!( p -  v,  p - &v);
+                prop_assert_eq!( p -  v, &p - &v);
+                prop_assert_eq!( p - &v, &p -  v);
+                prop_assert_eq!(&p -  v,  p - &v);
+                prop_assert_eq!(&p -  v, &p - &v);
+                prop_assert_eq!( p - &v, &p - &v);
+            }
+        }
+    }
+    }
+}
+
+approx_arithmetic_props!(
+    point1_f64_arithmetic_props, 
+    Point1, 
+    Vector1, 
+    f64, 
+    any_point1, 
+    any_vector1, 
+    1e-7
+);
+approx_arithmetic_props!(
+    point2_f64_arithmetic_props, 
+    Point2, 
+    Vector2, 
+    f64, 
+    any_point2, 
+    any_vector2, 
+    1e-7
+);
+approx_arithmetic_props!(
+    point3_f64_arithmetic_props, 
+    Point3, 
+    Vector3, 
+    f64, 
+    any_point3, 
+    any_vector3, 
+    1e-7
+);
+
+
+/// Generate property tests for point/vector arithmetic over integer scalars.
+///
+/// ### Macro Parameters
+///
+/// The macro parameters are the following:
+/// * `$TestModuleName` is a name we give to the module we place the property tests
+///    in to separate them from each other for each scalar type to prevent 
+///    namespace collisions.
+/// * `PointN` denotes the name of the point type.
+/// * `$VectorN` denotes the name of the vector type.
+/// * `$ScalarType` denotes the underlying system of numbers that compose the 
+///    set of vectors.
+/// * `$Generator` is the name of a function or closure for generating examples.
+macro_rules! exact_arithmetic_props {
+    ($TestModuleName:ident, $PointN:ident, $VectorN:ident, $ScalarType:ty, $PointGen:ident, $VectorGen:ident) => {
+    #[cfg(test)]
+    mod $TestModuleName {
+        use proptest::prelude::*;
+        use cglinalg::{
+            $VectorN,
+            Zero
+        };
+        use super::{
+            $PointGen,
+            $VectorGen,
+        };
+    
+
+        proptest! {
+            /// A point plus a zero vector equals the same point.
+            ///
+            /// Given a point `p` and a vector `0`
+            /// ```text
+            /// p + 0 = p
+            /// ```
+            #[test]
+            fn prop_point_plus_zero_equals_vector(p in $PointGen()) {
+                let zero_vec = $VectorN::<$ScalarType>::zero();
+
+                prop_assert_eq!(p + zero_vec, p);
+            }
+
+            /// Given a point `p` and a vector `v`, we should be able to use `p` and 
+            /// `v` interchangeably with their references `&p` and `&v` in 
+            /// arithmetic expressions involving points and vectors.
+            ///
+            /// Given a point `p` and a vector `v`, and their references `&p` and 
+            /// `&v`, they should satisfy
+            /// ```text
+            ///  p +  v = &p +  v
+            ///  p +  v =  p + &v
+            ///  p +  v = &p + &v
+            ///  p + &v = &p +  v
+            /// &p +  v =  p + &v
+            /// &p +  v = &p + &v
+            ///  p + &v = &p + &v
+            /// ```
+            #[test]
+            fn prop_point_plus_vector_equals_refpoint_plus_refvector(
+                p in $PointGen::<$ScalarType>(), v in $VectorGen::<$ScalarType>()) {
+                
+                prop_assert_eq!( p +  v, &p +  v);
+                prop_assert_eq!( p +  v,  p + &v);
+                prop_assert_eq!( p +  v, &p + &v);
+                prop_assert_eq!( p + &v, &p +  v);
+                prop_assert_eq!(&p +  v,  p + &v);
+                prop_assert_eq!(&p +  v, &p + &v);
+                prop_assert_eq!( p + &v, &p + &v);
+            }
+
+
+            /// Point and vector addition should be approximately compatible. 
+            /// 
+            /// Given a point `p` and vectors `v1` and `v2`, we have
+            /// ```text
+            /// (p + v1) + v2 = p + (v1 + v2)
+            /// ```
+            /// Note that floating point vector addition cannot be exactly 
+            /// associative because arithmetic with floating point numbers 
+            /// is not associative.
+            #[test]
+            fn prop_point_vector_addition_compatible(
+                p in $PointGen::<$ScalarType>(), 
+                v1 in $VectorGen::<$ScalarType>(), v2 in $VectorGen::<$ScalarType>()) {
+
+                prop_assert_eq!((p + v1) + v2, p + (v1 + v2));
+            }
+
+            /// A point minus a zero vector equals the same point.
+            ///
+            /// Given a point `p` and a vector `0`
+            /// ```text
+            /// p - 0 = p
+            /// ```
+            #[test]
+            fn prop_point_minus_zero_equals_vector(p in $PointGen()) {
+                let zero_vec = $VectorN::<$ScalarType>::zero();
+
+                prop_assert_eq!(p - zero_vec, p);
+            }
+
+            /// A point minus itself equals the zero vector.
+            ///
+            /// Given a point `p` and a vector `0`
+            /// ```text
+            /// p - p = 0
+            /// ```
+            #[test]
+            fn prop_point_minus_point_equals_zero_vector(p in $PointGen()) {
+                let zero_vec = $VectorN::<$ScalarType>::zero();
+
+                prop_assert_eq!(p - p, zero_vec);
+            }
+
+            /// Given a point `p` and a vector `v`, we should be able to use `p` and 
+            /// `v` interchangeably with their references `&p` and `&v` in 
+            /// arithmetic expressions involving points and vectors.
+            ///
+            /// Given a point `p` and a vector `v`, and their references `&p` and 
+            /// `&v`, they should satisfy
+            /// ```text
+            ///  p -  v = &p -  v
+            ///  p -  v =  p - &v
+            ///  p -  v = &p - &v
+            ///  p - &v = &p -  v
+            /// &p -  v =  p - &v
+            /// &p -  v = &p - &v
+            ///  p - &v = &p - &v
+            /// ```
+            #[test]
+            fn prop_point_minus_vector_equals_refpoint_plus_refvector(
+                p in $PointGen::<$ScalarType>(), v in $VectorGen::<$ScalarType>()) {
+                
+                prop_assert_eq!( p -  v, &p -  v);
+                prop_assert_eq!( p -  v,  p - &v);
+                prop_assert_eq!( p -  v, &p - &v);
+                prop_assert_eq!( p - &v, &p -  v);
+                prop_assert_eq!(&p -  v,  p - &v);
+                prop_assert_eq!(&p -  v, &p - &v);
+                prop_assert_eq!( p - &v, &p - &v);
+            }
+        }
+    }
+    }
+}
+
+exact_arithmetic_props!(point1_i64_arithmetic_props, Point1, Vector1, i64, any_point1, any_vector1);
+exact_arithmetic_props!(point2_i64_arithmetic_props, Point2, Vector2, i64, any_point2, any_vector2);
+exact_arithmetic_props!(point3_i64_arithmetic_props, Point3, Vector3, i64, any_point3, any_vector3);
+
