e67bcc0a81b900554e2b7697a221cf5a3c0e66bf
LambdaXymox <lambda.xymox@gmail.com>
2020-10-10 16:38:50 -0500

    Add more tests for rotation_between.

--- tests/test_rotation.rs
+++ tests/test_rotation.rs
@@ -61,11 +61,11 @@ mod rotation2_tests {
     }
 
     #[test]
-    fn test_rotation_between_vectors_point1() {
+    fn test_rotation_between_axis_vectors_point1() {
         let point = Point2::new(f64::sqrt(3_f64) / 2_f64, 1_f64 / 2_f64);
         let vector1 = Unit::from_value(Vector2::unit_y());
         let vector2 = Unit::from_value(Vector2::unit_x());
-        let rotation = Rotation2::rotation_between(&vector1, &vector2);
+        let rotation = Rotation2::rotation_between_axis(&vector1, &vector2);
         let expected = Point2::new(1_f64 / 2_f64, -f64::sqrt(3_f64) / 2_f64);
         let result = rotation.rotate_point(&point);
 
@@ -73,11 +73,11 @@ mod rotation2_tests {
     }
 
     #[test]
-    fn test_rotation_between_vectors_point2() {
+    fn test_rotation_between_axis_vectors_point2() {
         let point = Point2::new(0_f64, 1_f64);
         let vector1 = Unit::from_value(Vector2::unit_y());
         let vector2 = Unit::from_value(Vector2::unit_x());
-        let rotation = Rotation2::rotation_between(&vector1, &vector2);
+        let rotation = Rotation2::rotation_between_axis(&vector1, &vector2);
         let expected = Point2::new(1_f64, 0_f64);
         let result = rotation.rotate_point(&point);
 
@@ -85,11 +85,11 @@ mod rotation2_tests {
     }
 
     #[test]
-    fn test_rotation_between_vectors_vector1() {
+    fn test_rotation_between_axis_vectors_vector1() {
         let vector = Vector2::new(f64::sqrt(3_f64) / 2_f64, 1_f64 / 2_f64);
         let vector1 = Unit::from_value(Vector2::unit_y());
         let vector2 = Unit::from_value(Vector2::unit_x());
-        let rotation = Rotation2::rotation_between(&vector1, &vector2);
+        let rotation = Rotation2::rotation_between_axis(&vector1, &vector2);
         let expected = Vector2::new(1_f64 / 2_f64, -f64::sqrt(3_f64) / 2_f64);
         let result = rotation.rotate_vector(&vector);
 
@@ -97,12 +97,36 @@ mod rotation2_tests {
     }
 
     #[test]
-    fn test_rotation_between_vectors_vector2() {
+    fn test_rotation_between_axis_vectors_vector2() {
         let vector1: Unit<Vector2<f64>> = Unit::from_value(Vector2::unit_y());
         let vector2: Unit<Vector2<f64>> = Unit::from_value(Vector2::unit_x());
-        let rotation = Rotation2::rotation_between(&vector1, &vector2);
+        let rotation = Rotation2::rotation_between_axis(&vector1, &vector2);
         let vector = Vector2::unit_y();
         let expected = Vector2::unit_x();
+        let result = rotation.rotate_vector(&vector);
+
+        assert!(relative_eq!(result, expected, epsilon = 1e-8));
+    }
+
+    #[test]
+    fn test_rotation_between_vectors_point() {
+        let point = 3_f64 * Point2::new(0_f64, 1_f64);
+        let vector1 = 5_f64 * Vector2::unit_y();
+        let vector2 = 12_f64 * Vector2::unit_x();
+        let rotation = Rotation2::rotation_between(&vector1, &vector2);
+        let expected = 3_f64 * Point2::new(1_f64, 0_f64);
+        let result = rotation.rotate_point(&point);
+
+        assert!(relative_eq!(result, expected, epsilon = 1e-8));
+    }
+
+    #[test]
+    fn test_rotation_between_vectors_vector() {
+        let vector = 3_f64 * Vector2::new(f64::sqrt(3_f64) / 2_f64, 1_f64 / 2_f64);
+        let vector1 = 5_f64 * Vector2::unit_y();
+        let vector2 = 12_f64 * Vector2::unit_x();
+        let rotation = Rotation2::rotation_between(&vector1, &vector2);
+        let expected = 3_f64 * Vector2::new(1_f64 / 2_f64, -f64::sqrt(3_f64) / 2_f64);
         let result = rotation.rotate_vector(&vector);
 
         assert!(relative_eq!(result, expected, epsilon = 1e-8));
