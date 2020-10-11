be40a0fb34a797aad5ff5e8e9743a6a0675011df
LambdaXymox <lambda.xymox@gmail.com>
2020-10-10 23:42:01 -0500

    Add reflection tests.

--- tests/test_reflection.rs
+++ tests/test_reflection.rs
@@ -10,6 +10,9 @@ mod reflection2_tests {
         Unit,
         Zero,
     };
+    use approx::{
+        relative_eq,
+    };
 
 
     #[test]
@@ -59,4 +62,119 @@ mod reflection2_tests {
 
         assert_eq!(result, expected);
     }
+
+    /// Test the reflection through the line `y = x`.
+    /// Note that there is an ambiguity in the choice of normal to a line in
+    /// two dimensions. We can choose either a normal vector or its negation
+    /// to construct the reflection and get the same reflection.
+    #[test]
+    fn test_reflection_line_through_origin_point1() {
+        let normal: Unit<Vector2<f64>> = Unit::from_value(Vector2::new(
+            -1_f64 / f64::sqrt(2_f64), 
+             1_f64 / f64::sqrt(2_f64)
+        ));
+        let bias = Vector2::zero();
+        let reflection = Reflection2::from_normal_bias(&normal, &bias);
+        let point = Point2::new(2_f64, 1_f64);
+        let expected = Point2::new(1_f64, 2_f64);
+        let result = reflection.reflect_point(&point);
+
+        assert!(relative_eq!(result, expected, epsilon = 1e-8));
+    }
+
+    /// Test the reflection through the line `y = x`.
+    /// Note that there is an ambiguity in the choice of normal to a line in
+    /// two dimensions. We can choose either a normal vector or its negation
+    /// to construct the reflection and get the same reflection.
+    #[test]
+    fn test_reflection_line_through_origin_vector1() {
+        let normal: Unit<Vector2<f64>> = Unit::from_value(Vector2::new(
+            -1_f64 / f64::sqrt(2_f64), 
+             1_f64 / f64::sqrt(2_f64)
+        ));
+        let bias = Vector2::zero();
+        let reflection = Reflection2::from_normal_bias(&normal, &bias);
+        let vector = Vector2::new(2_f64, 1_f64);
+        let expected = Vector2::new(1_f64, 2_f64);
+        let result = reflection.reflect_vector(&vector);
+
+        assert!(relative_eq!(result, expected, epsilon = 1e-8));
+    }
+
+    /// Test the reflection through the line `y = x`.
+    /// Note that there is an ambiguity in the choice of normal to a line in
+    /// two dimensions. We can choose either a normal vector or its negation
+    /// to construct the reflection and get the same reflection.
+    #[test]
+    fn test_reflection_line_through_origin_point2() {
+        let normal: Unit<Vector2<f64>> = Unit::from_value(-Vector2::new(
+            -1_f64 / f64::sqrt(2_f64), 
+             1_f64 / f64::sqrt(2_f64)
+        ));
+        let bias = Vector2::zero();
+        let reflection = Reflection2::from_normal_bias(&normal, &bias);
+        let point = Point2::new(2_f64, 1_f64);
+        let expected = Point2::new(1_f64, 2_f64);
+        let result = reflection.reflect_point(&point);
+
+        assert!(relative_eq!(result, expected, epsilon = 1e-8));
+    }
+
+    /// Test the reflection through the line `y = x`.
+    /// Note that there is an ambiguity in the choice of normal to a line in
+    /// two dimensions. We can choose either a normal vector or its negation
+    /// to construct the reflection and get the same reflection.
+    #[test]
+    fn test_reflection_line_through_origin_vector2() {
+        let normal: Unit<Vector2<f64>> = Unit::from_value(-Vector2::new(
+            -1_f64 / f64::sqrt(2_f64), 
+             1_f64 / f64::sqrt(2_f64)
+        ));
+        let bias = Vector2::zero();
+        let reflection = Reflection2::from_normal_bias(&normal, &bias);
+        let vector = Vector2::new(2_f64, 1_f64);
+        let expected = Vector2::new(1_f64, 2_f64);
+        let result = reflection.reflect_vector(&vector);
+
+        assert!(relative_eq!(result, expected, epsilon = 1e-8));
+    }
+
+    /// Test the reflection through the line `y = x`.
+    /// Note that there is an ambiguity in the choice of normal to a line in
+    /// two dimensions. We can choose either a normal vector or its negation
+    /// to construct the reflection and get the same reflection.
+    #[test]
+    fn test_reflection_line_through_origin_point3() {
+        let normal: Unit<Vector2<f64>> = Unit::from_value(Vector2::new(
+            -1_f64 / f64::sqrt(2_f64), 
+             1_f64 / f64::sqrt(2_f64)
+        ));
+        let bias = Vector2::zero();
+        let reflection = Reflection2::from_normal_bias(&normal, &bias);
+        let point = Point2::new(2_f64, 2_f64);
+        let expected = Point2::new(2_f64, 2_f64);
+        let result = reflection.reflect_point(&point);
+
+        assert!(relative_eq!(result, expected, epsilon = 1e-8));
+    }
+
+    /// Test the reflection through the line `y = x`.
+    /// Note that there is an ambiguity in the choice of normal to a line in
+    /// two dimensions. We can choose either a normal vector or its negation
+    /// to construct the reflection and get the same reflection.
+    #[test]
+    fn test_reflection_line_through_origin_vector3() {
+        let normal: Unit<Vector2<f64>> = Unit::from_value(Vector2::new(
+            -1_f64 / f64::sqrt(2_f64), 
+             1_f64 / f64::sqrt(2_f64)
+        ));
+        let bias = Vector2::zero();
+        let reflection = Reflection2::from_normal_bias(&normal, &bias);
+        let vector = Vector2::new(2_f64, 2_f64);
+        let expected = Vector2::new(2_f64, 2_f64);
+        let result = reflection.reflect_vector(&vector);
+
+        assert!(relative_eq!(result, expected, epsilon = 1e-8));
+    }
 }
+
