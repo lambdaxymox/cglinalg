964856bf9b682f2abe3289eab073799ffa6cce53
LambdaXymox <lambda.xymox@gmail.com>
2020-10-11 00:23:24 -0500

    Add more tests for reflection.

--- tests/test_reflection.rs
+++ tests/test_reflection.rs
@@ -176,5 +176,37 @@ mod reflection2_tests {
 
         assert!(relative_eq!(result, expected, epsilon = 1e-8));
     }
+
+    /// Test the reflection through the line `y = (1/2)*x + 1`.
+    #[test]
+    fn test_reflection_arbitrary_line_point() {
+        let normal: Unit<Vector2<f64>> = Unit::from_value(Vector2::new(
+            -1_f64 / 2_f64, 
+             1_f64
+        ));
+        let bias = Vector2::new(0_f64, 1_f64);
+        let reflection = Reflection2::from_normal_bias(&normal, &bias);
+        let point = Point2::new(1_f64, 1_f64);
+        let expected = Point2::new(3_f64 / 5_f64, 9_f64 / 5_f64);
+        let result = reflection.reflect_point(&point);
+
+        assert!(relative_eq!(result, expected, epsilon = 1e-8));
+    }
+
+    /// Test the reflection through the line `y = (1/2)*x + 1.
+    #[test]
+    fn test_reflection_arbitrary_line_vector() {
+        let normal: Unit<Vector2<f64>> = Unit::from_value(Vector2::new(
+            -1_f64 / 2_f64, 
+             1_f64
+        ));
+        let bias = Vector2::new(0_f64, 1_f64);
+        let reflection = Reflection2::from_normal_bias(&normal, &bias);
+        let vector = Vector2::new(1_f64, 1_f64);
+        let expected = Vector2::new(7_f64 / 5_f64, 1_f64 / 5_f64);
+        let result = reflection.reflect_vector(&vector);
+
+        assert!(relative_eq!(result, expected, epsilon = 1e-8));
+    }
 }
 
