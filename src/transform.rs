d5e691a90545762f40171fa17c90b6f712e4914e
LambdaXymox <lambda.xymox@gmail.com>
2020-10-17 02:25:28 -0500

    Add documentation to transform module.

--- src/transform.rs
+++ src/transform.rs
@@ -43,6 +43,15 @@ impl<S> Transform2<S> where S: Scalar {
         }
     }
 
+    /// Construct a new transformation from a given homogeneous matrix. The input
+    /// matrix is not checked that it is a valid homogeneous matrix.
+    #[inline]
+    pub fn from_matrix_unchecked(matrix: Matrix3x3<S>) -> Transform2<S> {
+        Transform2 {
+            matrix: matrix,
+        }
+    }
+
     /// Get a reference to the underlying matrix that represents the 
     /// transformation.
     #[inline]
@@ -59,6 +68,20 @@ impl<S> Transform2<S> where S: Scalar {
 
     /// The identity transformation for a generic two-dimensional 
     /// transformation.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use cglinalg::{
+    /// #     Transform2,
+    /// #     Point2,
+    /// # };
+    /// #
+    /// let transform = Transform2::identity();
+    /// let point = Point2::new(1_f64, 2_f64);
+    ///
+    /// assert_eq!(transform * point, point);
+    /// ```
     #[inline]
     pub fn identity() -> Transform2<S> {
         Transform2 { 
@@ -69,6 +92,26 @@ impl<S> Transform2<S> where S: Scalar {
 
 impl<S> Transform2<S> where S: ScalarFloat {
     /// Compute the inverse of the transformation if it exists.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use cglinalg::{
+    /// #     Transform2,
+    /// #     Point2,
+    /// #     Matrix3x3,
+    /// # };
+    /// #
+    /// let matrix = Matrix3x3::new(
+    ///     0_f64, -1_f64, 0_f64,
+    ///     1_f64,  0_f64, 0_f64,
+    ///     2_f64,  3_f64, 1_f64  
+    /// );
+    /// let transform = Transform2::from_matrix_unchecked(matrix);
+    /// let transform_inv = transform.inverse().unwrap();
+    ///
+    /// assert_eq!(transform * transform_inv, Transform2::identity());
+    /// ```
     #[inline]
     pub fn inverse(&self) -> Option<Transform2<S>> {
         if let Some(matrix) = self.matrix.inverse() {
@@ -81,18 +124,85 @@ impl<S> Transform2<S> where S: ScalarFloat {
     }
 
     /// Apply the transformation to a vector.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use cglinalg::{
+    /// #     Transform2,
+    /// #     Vector2,
+    /// #     Matrix3x3, 
+    /// # };
+    /// #
+    /// let matrix = Matrix3x3::new(
+    ///     6_f64, 7_f64, 0_f64,
+    ///     8_f64, 9_f64, 0_f64,
+    ///     2_f64, 3_f64, 1_f64,   
+    /// );
+    /// let transform = Transform2::from_matrix_unchecked(matrix);
+    /// let vector = Vector2::new(1_f64, 2_f64);
+    /// let expected = Vector2::new(22_f64, 25_f64);
+    /// let result = transform.transform_vector(&vector);
+    ///
+    /// assert_eq!(result, expected);
+    /// ```
     #[inline]
     pub fn transform_vector(&self, vector: &Vector2<S>) -> Vector2<S> {
         (self.matrix * vector.expand(S::zero())).contract()
     }
 
-    /// Apply the inverse of the transformation to a point.
+    /// Apply the transformation to a point.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use cglinalg::{
+    /// #     Transform2,
+    /// #     Point2,
+    /// #     Matrix3x3, 
+    /// # };
+    /// #
+    /// let matrix = Matrix3x3::new(
+    ///     6_f64, 7_f64, 0_f64,
+    ///     8_f64, 9_f64, 0_f64,
+    ///     2_f64, 3_f64, 1_f64,   
+    /// );
+    /// let transform = Transform2::from_matrix_unchecked(matrix);
+    /// let point = Point2::new(1_f64, 2_f64);
+    /// let expected = Point2::new(24_f64, 28_f64);
+    /// let result = transform.transform_point(&point);
+    ///
+    /// assert_eq!(result, expected);
+    /// ```
     #[inline]
     pub fn transform_point(&self, point: &Point2<S>) -> Point2<S> {
         Point2::from_homogeneous(self.matrix * point.to_homogeneous()).unwrap()
     }
 
     /// Apply the inverse of the transformation to a vector.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use cglinalg::{
+    /// #     Transform2,
+    /// #     Vector2,
+    /// #     Matrix3x3, 
+    /// # };
+    /// #
+    /// let matrix = Matrix3x3::new(
+    ///     6_f64, 7_f64, 0_f64,
+    ///     8_f64, 9_f64, 0_f64,
+    ///     2_f64, 3_f64, 1_f64,   
+    /// );
+    /// let transform = Transform2::from_matrix_unchecked(matrix);
+    /// let vector = Vector2::new(1_f64, 2_f64);
+    /// let expected = Some(vector);
+    /// let transformed_vector = transform.transform_vector(&vector);
+    /// let result = transform.inverse_transform_vector(&transformed_vector);
+    ///
+    /// assert_eq!(result, expected);
+    /// ```
     #[inline]
     pub fn inverse_transform_vector(&self, vector: &Vector2<S>) -> Option<Vector2<S>> {
         self.inverse()
@@ -100,6 +210,29 @@ impl<S> Transform2<S> where S: ScalarFloat {
     }
 
     /// Apply the inverse of the transformation to a point.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use cglinalg::{
+    /// #     Transform2,
+    /// #     Point2,
+    /// #     Matrix3x3, 
+    /// # };
+    /// #
+    /// let matrix = Matrix3x3::new(
+    ///     6_f64, 7_f64, 0_f64,
+    ///     8_f64, 9_f64, 0_f64,
+    ///     2_f64, 3_f64, 1_f64,   
+    /// );
+    /// let transform = Transform2::from_matrix_unchecked(matrix);
+    /// let point = Point2::new(1_f64, 2_f64);
+    /// let expected = Some(point);
+    /// let transformed_point = transform.transform_point(&point);
+    /// let result = transform.inverse_transform_point(&transformed_point);
+    ///
+    /// assert_eq!(result, expected);
+    /// ```
     #[inline]
     pub fn inverse_transform_point(&self, point: &Point2<S>) -> Option<Point2<S>> {
         self.inverse()
@@ -212,6 +345,50 @@ impl<'a, 'b, S> ops::Mul<&'a Point2<S>> for &'b Transform2<S> where S: ScalarFlo
     }
 }
 
+impl<S> ops::Mul<Transform2<S>> for Transform2<S> where S: ScalarFloat {
+    type Output = Transform2<S>;
+
+    #[inline]
+    fn mul(self, other: Transform2<S>) -> Self::Output {
+        Transform2 {
+            matrix: &self.matrix * &other.matrix
+        }
+    }
+}
+
+impl<S> ops::Mul<&Transform2<S>> for Transform2<S> where S: ScalarFloat {
+    type Output = Transform2<S>;
+
+    #[inline]
+    fn mul(self, other: &Transform2<S>) -> Self::Output {
+        Transform2 {
+            matrix: &self.matrix * &other.matrix
+        }
+    }
+}
+
+impl<S> ops::Mul<Transform2<S>> for &Transform2<S> where S: ScalarFloat {
+    type Output = Transform2<S>;
+
+    #[inline]
+    fn mul(self, other: Transform2<S>) -> Self::Output {
+        Transform2 {
+            matrix: &self.matrix * &other.matrix
+        }
+    }
+}
+
+impl<'a, 'b, S> ops::Mul<&'a Transform2<S>> for &'b Transform2<S> where S: ScalarFloat {
+    type Output = Transform2<S>;
+
+    #[inline]
+    fn mul(self, other: &'a Transform2<S>) -> Self::Output {
+        Transform2 {
+            matrix: &self.matrix * &other.matrix
+        }
+    }
+}
+
 
 /// A generic three-dimensional transformation.
 #[repr(transparent)]
@@ -233,6 +410,15 @@ impl<S> Transform3<S> where S: Scalar {
         }
     }
 
+    /// Construct a new transformation from a given homogeneous matrix. The input
+    /// matrix is not checked that it is a valid homogeneous matrix.
+    #[inline]
+    pub fn from_matrix_unchecked(matrix: Matrix4x4<S>) -> Transform3<S> {
+        Transform3 {
+            matrix: matrix,
+        }
+    }
+
     /// Get a reference to the underlying matrix that represents the 
     /// transformation.
     #[inline]
@@ -249,6 +435,20 @@ impl<S> Transform3<S> where S: Scalar {
 
     /// The identity transformation for a generic three-dimensional 
     /// transformation.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use cglinalg::{
+    /// #     Transform3,
+    /// #     Point3,
+    /// # };
+    /// #
+    /// let transform = Transform3::identity();
+    /// let point = Point3::new(1_f64, 2_f64, 3_f64);
+    ///
+    /// assert_eq!(transform * point, point);
+    /// ```
     #[inline]
     pub fn identity() -> Transform3<S> {
         Transform3 { 
@@ -259,6 +459,27 @@ impl<S> Transform3<S> where S: Scalar {
 
 impl<S> Transform3<S> where S: ScalarFloat {
     /// Compute the inverse of the transformation if it exists.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use cglinalg::{
+    /// #     Transform3,
+    /// #     Point3,
+    /// #     Matrix4x4,
+    /// # };
+    /// #
+    /// let matrix = Matrix4x4::new(
+    ///     0_f64, -1_f64, 0_f64, 0_f64,
+    ///     1_f64,  0_f64, 0_f64, 0_f64,
+    ///     0_f64,  0_f64, 1_f64, 0_f64,
+    ///     2_f64,  3_f64, 4_f64, 1_f64  
+    /// );
+    /// let transform = Transform3::from_matrix_unchecked(matrix);
+    /// let transform_inv = transform.inverse().unwrap();
+    ///
+    /// assert_eq!(transform * transform_inv, Transform3::identity());
+    /// ```
     #[inline]
     pub fn inverse(&self) -> Option<Transform3<S>> {
         if let Some(matrix) = self.matrix.inverse() {
@@ -271,12 +492,58 @@ impl<S> Transform3<S> where S: ScalarFloat {
     }
 
     /// Apply the transformation to a vector.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use cglinalg::{
+    /// #     Transform3,
+    /// #     Vector3,
+    /// #     Matrix4x4, 
+    /// # };
+    /// #
+    /// let matrix = Matrix4x4::new(
+    ///     5_f64,  6_f64,  5_f64, 0_f64,
+    ///     2_f64,  5_f64,  8_f64, 0_f64,
+    ///     12_f64, 15_f64, 9_f64, 0_f64, 
+    ///     6_f64,  6_f64,  6_f64, 1_f64
+    /// );
+    /// let transform = Transform3::from_matrix_unchecked(matrix);
+    /// let vector = Vector3::new(1_f64, 2_f64, 3_f64);
+    /// let expected = Vector3::new(45_f64, 61_f64, 48_f64);
+    /// let result = transform.transform_vector(&vector);
+    /// 
+    /// assert_eq!(result, expected);
+    /// ```
     #[inline]
     pub fn transform_vector(&self, vector: &Vector3<S>) -> Vector3<S> {
         (self.matrix * vector.expand(S::zero())).contract()
     }
 
     /// Apply the inverse of the transformation to a point.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use cglinalg::{
+    /// #     Transform3,
+    /// #     Point3,
+    /// #     Matrix4x4, 
+    /// # };
+    /// #
+    /// let matrix = Matrix4x4::new(
+    ///     5_f64,  6_f64,  5_f64, 0_f64,
+    ///     2_f64,  5_f64,  8_f64, 0_f64,
+    ///     12_f64, 15_f64, 9_f64, 0_f64, 
+    ///     6_f64,  6_f64,  6_f64, 1_f64
+    /// );
+    /// let transform = Transform3::from_matrix_unchecked(matrix);
+    /// let point = Point3::new(1_f64, 2_f64, 3_f64);
+    /// let expected = Point3::new(51_f64, 67_f64, 54_f64);
+    /// let result = transform.transform_point(&point);
+    /// 
+    /// assert_eq!(result, expected);
+    /// ```
     #[inline]
     pub fn transform_point(&self, point: &Point3<S>) -> Point3<S> {
         Point3::from_homogeneous(self.matrix * point.to_homogeneous()).unwrap()
@@ -399,6 +666,50 @@ impl<'a, 'b, S> ops::Mul<&'a Point3<S>> for &'b Transform3<S> where S: ScalarFlo
     #[inline]
     fn mul(self, other: &'a Point3<S>) -> Self::Output {
         self.transform_point(other)
+    }
+}
+
+impl<S> ops::Mul<Transform3<S>> for Transform3<S> where S: ScalarFloat {
+    type Output = Transform3<S>;
+
+    #[inline]
+    fn mul(self, other: Transform3<S>) -> Self::Output {
+        Transform3 {
+            matrix: &self.matrix * &other.matrix
+        }
+    }
+}
+
+impl<S> ops::Mul<&Transform3<S>> for Transform3<S> where S: ScalarFloat {
+    type Output = Transform3<S>;
+
+    #[inline]
+    fn mul(self, other: &Transform3<S>) -> Self::Output {
+        Transform3 {
+            matrix: &self.matrix * &other.matrix
+        }
+    }
+}
+
+impl<S> ops::Mul<Transform3<S>> for &Transform3<S> where S: ScalarFloat {
+    type Output = Transform3<S>;
+
+    #[inline]
+    fn mul(self, other: Transform3<S>) -> Self::Output {
+        Transform3 {
+            matrix: &self.matrix * &other.matrix
+        }
+    }
+}
+
+impl<'a, 'b, S> ops::Mul<&'a Transform3<S>> for &'b Transform3<S> where S: ScalarFloat {
+    type Output = Transform3<S>;
+
+    #[inline]
+    fn mul(self, other: &'a Transform3<S>) -> Self::Output {
+        Transform3 {
+            matrix: &self.matrix * &other.matrix
+        }
     }
 }
 
