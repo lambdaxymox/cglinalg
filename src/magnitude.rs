5b169c0095ef1d269f32aff6a61a7a6918b269c7
LambdaXymox <lambda.xymox@gmail.com>
2020-10-19 17:02:29 -0500

    Remove dead code.

--- src/magnitude.rs
+++ src/magnitude.rs
@@ -6,22 +6,6 @@ use num_traits::{
     Float,
 };
 
-/*
-/// A type with this trait has a notion of comparing the distance (metric) 
-/// between two elements of that type. For example, one can use this trait 
-/// to compute the Euclidean distance between two vectors. 
-pub trait Metric {
-    type Output: ScalarFloat;
-
-    /// Compute the squared Eucliean distance between two vectors.
-    fn distance_squared(&self, other: &Self) -> Self::Output;
-
-    /// Compute the Euclidean distance between two vectors.
-    fn distance(&self, other: &Self) -> Self::Output {
-        Self::Output::sqrt(Self::distance_squared(&self, other))
-    }
-}
-*/
 /// This trait enables one to assign lengths to vectors.
 pub trait Magnitude where Self: Sized {
     type Output: ScalarFloat;
