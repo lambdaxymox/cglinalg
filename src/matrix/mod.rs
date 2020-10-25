87beb833078cd8dfff9825b262cbb5608ef4ea16
LambdaXymox <lambda.xymox@gmail.com>
2020-10-25 18:58:36 -0500

    Rename matrix_binary_ops to matrix_scalar_binary_ops.

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -1,14 +1,14 @@
 mod matrix;
-mod matrix_binary_ops;
 mod matrix_interpolate;
 mod matrix_matrix_binary_ops;
+mod matrix_scalar_binary_ops;
 mod matrix_unary_ops;
 mod scalar_matrix_mul_ops;
 
 pub use matrix::*;
-pub use matrix_binary_ops::*;
 pub use matrix_interpolate::*;
 pub use matrix_matrix_binary_ops::*;
+pub use matrix_scalar_binary_ops::*;
 pub use matrix_unary_ops::*;
 pub use scalar_matrix_mul_ops::*;
 
