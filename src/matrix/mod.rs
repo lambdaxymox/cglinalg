74931a677bdbb78a98a5c42f7bbaad4f92253692
LambdaXymox <lambda.xymox@gmail.com>
2020-10-25 18:46:35 -0500

    Move matrix matrix binary ops to a dedicated file.

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -1,10 +1,12 @@
 mod matrix;
 mod matrix_binary_ops;
+mod matrix_matrix_binary_ops;
 mod matrix_unary_ops;
 mod matrix_interpolate;
 
 pub use matrix::*;
 pub use matrix_binary_ops::*;
+pub use matrix_matrix_binary_ops::*;
 pub use matrix_unary_ops::*;
 pub use matrix_interpolate::*;
 
