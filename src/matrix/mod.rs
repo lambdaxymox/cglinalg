32d55f65f25bb066b93cfa0fd7230ffbd81452d2
LambdaXymox <lambda.xymox@gmail.com>
2020-10-25 18:53:34 -0500

    Move matrix/scalar binary operations to a dedicated file.

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -1,12 +1,14 @@
 mod matrix;
 mod matrix_binary_ops;
+mod matrix_interpolate;
 mod matrix_matrix_binary_ops;
 mod matrix_unary_ops;
-mod matrix_interpolate;
+mod scalar_matrix_mul_ops;
 
 pub use matrix::*;
 pub use matrix_binary_ops::*;
+pub use matrix_interpolate::*;
 pub use matrix_matrix_binary_ops::*;
 pub use matrix_unary_ops::*;
-pub use matrix_interpolate::*;
+pub use scalar_matrix_mul_ops::*;
 
