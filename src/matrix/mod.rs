2c0d3b2e82c33f2e5258bb83ccf990cf05a60dae
LambdaXymox <lambda.xymox@gmail.com>
2020-10-25 19:31:13 -0500

    Rename a couple modules with shorter names.

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -1,16 +1,16 @@
 mod matrix;
 mod matrix_assign_ops;
 mod matrix_interpolate;
-mod matrix_matrix_binary_ops;
-mod matrix_scalar_binary_ops;
+mod matrix_matrix_ops;
+mod matrix_scalar_ops;
 mod matrix_unary_ops;
 mod scalar_matrix_mul_ops;
 
 pub use matrix::*;
 pub use matrix_assign_ops::*;
 pub use matrix_interpolate::*;
-pub use matrix_matrix_binary_ops::*;
-pub use matrix_scalar_binary_ops::*;
+pub use matrix_matrix_ops::*;
+pub use matrix_scalar_ops::*;
 pub use matrix_unary_ops::*;
 pub use scalar_matrix_mul_ops::*;
 
