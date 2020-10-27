8078635f9bf82fa8936aab9fc006f84a664ccfee
LambdaXymox <lambda.xymox@gmail.com>
2020-10-27 15:17:10 -0500

    Move scalar / matrix multiplication operations to matrix module.

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -3,7 +3,6 @@ mod matrix;
 mod matrix_assign_ops;
 mod matrix_interpolate;
 mod matrix_unary_ops;
-mod scalar_matrix_mul_ops;
 mod matrix_vector_mul_ops;
 mod matrix_matrix_mul_ops;
 
@@ -11,7 +10,6 @@ pub use matrix::*;
 pub use matrix_assign_ops::*;
 pub use matrix_interpolate::*;
 pub use matrix_unary_ops::*;
-pub use scalar_matrix_mul_ops::*;
 pub use matrix_vector_mul_ops::*;
 pub use matrix_matrix_mul_ops::*;
 
