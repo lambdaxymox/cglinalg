6dc93f2365b4775e18ddcfdfa8bc23ce386a42c0
LambdaXymox <lambda.xymox@gmail.com>
2020-10-27 15:24:30 -0500

    Move matrix unary operations to matrix module.

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -2,14 +2,12 @@ mod array;
 mod matrix;
 mod matrix_assign_ops;
 mod matrix_interpolate;
-mod matrix_unary_ops;
 mod matrix_vector_mul_ops;
 mod matrix_matrix_mul_ops;
 
 pub use matrix::*;
 pub use matrix_assign_ops::*;
 pub use matrix_interpolate::*;
-pub use matrix_unary_ops::*;
 pub use matrix_vector_mul_ops::*;
 pub use matrix_matrix_mul_ops::*;
 
