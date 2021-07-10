88dd10d5932d4a3a23cc180bb14cb684c9a3a725
LambdaXymox <lambda.xymox@gmail.com>
2021-07-10 12:09:45 -0500

    Add scale multiplication tests.

--- tests/test_scale.rs
+++ tests/test_scale.rs
@@ -101,3 +101,39 @@ mod scale3_tests {
     }
 }
 
+#[cfg(test)]
+mod scale2_composition_tests {
+    use cglinalg::{
+        Scale2,
+    };
+
+
+    #[test]
+    fn test_scale_multiplication_nonuniform() {
+        let scale1 = Scale2::from_nonuniform_scale(1_f64, 2_f64);
+        let scale2 = Scale2::from_nonuniform_scale(3_f64, 4_f64);
+        let expected = Scale2::from_nonuniform_scale(3_f64, 8_f64);
+        let result = scale1 * scale2;
+
+        assert_eq!(result, expected);
+    }
+}
+
+#[cfg(test)]
+mod scale3_composition_tests {
+    use cglinalg::{
+        Scale3,
+    };
+
+
+    #[test]
+    fn test_scale_multiplication_nonuniform() {
+        let scale1 = Scale3::from_nonuniform_scale(1_f64, 2_f64, 10_f64);
+        let scale2 = Scale3::from_nonuniform_scale(3_f64, 4_f64, 32_f64);
+        let expected = Scale3::from_nonuniform_scale(3_f64, 8_f64, 320_f64);
+        let result = scale1 * scale2;
+
+        assert_eq!(result, expected);
+    }
+}
+
