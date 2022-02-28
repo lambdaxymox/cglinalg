2037fd725bcb549417f5d8a7125d580370f66f77
LambdaXymox <lambda.xymox@gmail.com>
2022-02-28 17:45:41 -0600

    Add indexing and array manipulation functions.

--- src/complex.rs
+++ src/complex.rs
@@ -26,6 +26,29 @@ impl<S> Complex<S> {
             im: im,
         }   
     }
+
+    #[inline]
+    pub fn shape(&self) -> (usize, usize) {
+        (2, 1)
+    }
+
+    /// Get a pointer to the underlying array.
+    #[inline]
+    pub fn as_ptr(&self) -> *const S {
+        &self.re
+    }
+
+    /// Get a mutable pointer to the underlying array.
+    #[inline]
+    pub fn as_mut_ptr(&mut self) -> *mut S {
+        &mut self.re
+    }
+
+    /// Get a slice of the underlying elements of the data type.
+    #[inline]
+    pub fn as_slice(&self) -> &[S] {
+        <Self as AsRef<[S; 2]>>::as_ref(self)
+    }
 }
 
 impl<S> Complex<S>
@@ -125,18 +148,18 @@ where
     }
 
     #[inline]
-    pub fn from_polar<A: Into<Radians<S>>>(radius: S, angle: A) -> Self {
+    pub fn from_polar_decomposition<A: Into<Radians<S>>>(radius: S, angle: A) -> Self {
         let _angle: Radians<S> = angle.into();
         Self::new(radius * _angle.cos(), radius * _angle.sin())
     }
 
     #[inline]
     pub fn from_angle<A: Into<Radians<S>>>(angle: A) -> Self {
-        Self::from_polar(S::one(), angle)
+        Self::from_polar_decomposition(S::one(), angle)
     }
 
     #[inline]
-    pub fn to_polar(self) -> (S, Radians<S>) {
+    pub fn to_polar_decomposition(self) -> (S, Radians<S>) {
         (self.magnitude(), Radians(self.arg()))
     }
 
@@ -166,6 +189,162 @@ where
         let (sin_angle_over_two, cos_angle_over_two) = (angle / two).sin_cos();
 
         Self::new(magnitude * cos_angle_over_two, magnitude * sin_angle_over_two)
+    }
+}
+
+impl<S> AsRef<[S; 2]> for Complex<S> {
+    #[inline]
+    fn as_ref(&self) -> &[S; 2] {
+        unsafe { 
+            &*(self as *const Complex<S> as *const [S; 2])
+        }
+    }
+}
+
+impl<S> AsRef<(S, S)> for Complex<S> {
+    #[inline]
+    fn as_ref(&self) -> &(S, S) {
+        unsafe { 
+            &*(self as *const Complex<S> as *const (S, S))
+        }
+    }
+}
+
+impl<S> AsMut<[S; 2]> for Complex<S> {
+    #[inline]
+    fn as_mut(&mut self) -> &mut [S; 2] {
+        unsafe { 
+            &mut *(self as *mut Complex<S> as *mut [S; 2])
+        }
+    }
+}
+
+impl<S> AsMut<(S, S)> for Complex<S> {
+    #[inline]
+    fn as_mut(&mut self) -> &mut (S, S) {
+        unsafe { 
+            &mut *(self as *mut Complex<S> as *mut (S, S))
+        }
+    }
+}
+
+impl<S> ops::Index<usize> for Complex<S> 
+where 
+    S: Scalar 
+{
+    type Output = S;
+
+    #[inline]
+    fn index(&self, index: usize) -> &Self::Output {
+        let v: &[S; 2] = self.as_ref();
+        &v[index]
+    }
+}
+
+impl<S> ops::Index<ops::Range<usize>> for Complex<S> 
+where 
+    S: Scalar 
+{
+    type Output = [S];
+
+    #[inline]
+    fn index(&self, index: ops::Range<usize>) -> &Self::Output {
+        let v: &[S; 2] = self.as_ref();
+        &v[index]
+    }
+}
+
+impl<S> ops::Index<ops::RangeTo<usize>> for Complex<S> 
+where 
+    S: Scalar 
+{
+    type Output = [S];
+
+    #[inline]
+    fn index(&self, index: ops::RangeTo<usize>) -> &Self::Output {
+        let v: &[S; 2] = self.as_ref();
+        &v[index]
+    }
+}
+
+impl<S> ops::Index<ops::RangeFrom<usize>> for Complex<S> 
+where 
+    S: Scalar 
+{
+    type Output = [S];
+
+    #[inline]
+    fn index(&self, index: ops::RangeFrom<usize>) -> &Self::Output {
+        let v: &[S; 2] = self.as_ref();
+        &v[index]
+    }
+}
+
+impl<S> ops::Index<ops::RangeFull> for Complex<S> 
+where 
+    S: Scalar 
+{
+    type Output = [S];
+
+    #[inline]
+    fn index(&self, index: ops::RangeFull) -> &Self::Output {
+        let v: &[S; 2] = self.as_ref();
+        &v[index]
+    }
+}
+
+impl<S> ops::IndexMut<usize> for Complex<S> 
+where 
+    S: Scalar 
+{
+    #[inline]
+    fn index_mut(&mut self, index: usize) -> &mut S {
+        let v: &mut [S; 2] = self.as_mut();
+        &mut v[index]
+    }
+}
+
+impl<S> ops::IndexMut<ops::Range<usize>> for Complex<S> 
+where 
+    S: Scalar 
+{
+    #[inline]
+    fn index_mut(&mut self, index: ops::Range<usize>) -> &mut [S] {
+        let v: &mut [S; 2] = self.as_mut();
+        &mut v[index]
+    }
+}
+
+impl<S> ops::IndexMut<ops::RangeTo<usize>> for Complex<S> 
+where 
+    S: Scalar 
+{
+    #[inline]
+    fn index_mut(&mut self, index: ops::RangeTo<usize>) -> &mut [S] {
+        let v: &mut [S; 2] = self.as_mut();
+        &mut v[index]
+    }
+}
+
+impl<S> ops::IndexMut<ops::RangeFrom<usize>> for Complex<S> 
+where 
+    S: Scalar 
+{
+    #[inline]
+    fn index_mut(&mut self, index: ops::RangeFrom<usize>) -> &mut [S] {
+        let v: &mut [S; 2] = self.as_mut();
+        &mut v[index]
+    }
+}
+
+impl<S> ops::IndexMut<ops::RangeFull> for Complex<S> 
+where 
+    S: Scalar 
+{
+    #[inline]
+    fn index_mut(&mut self, index: ops::RangeFull) -> &mut [S] {
+        let v: &mut [S; 2] = self.as_mut();
+        &mut v[index]
     }
 }
 
