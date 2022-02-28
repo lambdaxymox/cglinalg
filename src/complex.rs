b61b95258110be2ecafc2efdf7f7416e99bd3b3a
LambdaXymox <lambda.xymox@gmail.com>
2022-02-28 15:39:34 -0600

    Add inverse and conjugate functions for complex numbers.

--- src/complex.rs
+++ src/complex.rs
@@ -1,15 +1,17 @@
 use crate::base::{
+    Magnitude,
     Scalar,
     ScalarSigned,
     ScalarFloat,
 };
+use core::ops;
 
 
 #[repr(C)]
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Complex<S> {
-    re: S,
-    im: S,
+    pub re: S,
+    pub im: S,
 }
 
 impl<S> Complex<S> {
@@ -24,11 +26,73 @@ impl<S> Complex<S> {
 
 impl<S> Complex<S>
 where
-    S: Scalar
+    S: Clone + num_traits::Num
 {
     #[inline]
     pub fn i() -> Self {
         Self::new(S::zero(), S::one())
     }
 
+    #[inline]
+    pub fn magnitude_squared(&self) -> S {
+        self.re.clone() * self.re.clone() + self.im.clone() * self.im.clone()
+    }
 }
+
+impl<S> Complex<S>
+where
+    S: Clone + num_traits::Num + ops::Neg<Output = S>
+{
+    #[inline]
+    pub fn conjugate(&self) -> Self {
+        Self::new(self.re.clone(), -self.im.clone())
+    }
+
+    #[inline]
+    pub fn scale(&self, scale: S) -> Self {
+        Self::new(
+            self.re.clone() * scale.clone(),
+            self.im.clone() * scale
+        )
+    }
+
+    #[inline]
+    pub fn unscale(&self, scale: S) -> Self {
+        let one_over_scale = S::one() / scale;
+
+        Self::new(
+            self.re.clone() * one_over_scale.clone(), 
+            self.im.clone() * one_over_scale
+        )
+    }
+
+    #[inline]
+    pub fn inverse(&self) -> Self {
+        let magnitude_squared = self.magnitude_squared();
+        Self::new(
+             self.re.clone() / magnitude_squared.clone(),
+            -self.im.clone() / magnitude_squared
+        )
+    }
+
+    #[inline]
+    pub fn powi(&self, power: i32) -> Self {
+        unimplemented!()
+    }
+
+    #[inline]
+    pub fn powu(&self, power: u32) -> Self {
+        unimplemented!()
+    }
+}
+
+impl<S> Complex<S>
+where
+    S: num_traits::Float
+{
+    #[inline]
+    pub fn magnitude(&self) -> S {
+        self.magnitude_squared().sqrt()
+    }
+}
+
