4f3cf8c5b49bdb997727682509778819449a0f60
LambdaXymox <lambda.xymox@gmail.com>
2022-03-03 15:57:18 -0600

    Add properties to complex number specification.

--- tests/spec_complex.rs
+++ tests/spec_complex.rs
@@ -337,3 +337,210 @@ macro_rules! approx_add_props {
 approx_add_props!(complex_f64_add_props, f64, any_complex, 1e-7);
 
 
+/// Generate property tests for complex number arithmetic over exact scalars.
+///
+/// ### Macro Parameters
+///
+/// The macro parameters are the following:
+/// * `$TestModuleName` is a name we give to the module we place the property 
+///    tests in to separate them from each other for each scalar type to prevent 
+///    namespace collisions.
+/// * `$ScalarType` denotes the underlying system of numbers that compose the 
+///    set of complex numbers.
+/// * `$Generator` is the name of a function or closure for generating examples.
+macro_rules! exact_add_props {
+    ($TestModuleName:ident, $ScalarType:ty, $Generator:ident) => {
+    #[cfg(test)]
+    mod $TestModuleName {
+        use proptest::prelude::*;
+        use cglinalg::{
+            Complex, 
+        };
+        use super::{
+            $Generator,
+        };
+
+
+        proptest! {
+            /// A complex number plus a zero complex number equals the same complex.
+            ///
+            /// Given a complex number `z`, it should satisfy
+            /// ```text
+            /// z + 0 = z
+            /// ```
+            #[test]
+            fn prop_complex_plus_zero_equals_complex(z in $Generator()) {
+                let zero_complex = Complex::<$ScalarType>::zero();
+
+                prop_assert_eq!(z + zero_complex, z);
+            }
+
+            /// A zero complex number plus a complex number equals the same complex number.
+            ///
+            /// Given a complex number `z`, it should satisfy
+            /// ```text
+            /// 0 + z = z
+            /// ```
+            #[test]
+            fn prop_zero_plus_complex_equals_complex(z in $Generator()) {
+                let zero_complex = Complex::<$ScalarType>::zero();
+
+                prop_assert_eq!(zero_complex + z, z);
+            }
+
+            /// Given complex numbers `z1` and `z2`, we should be able to use `z1` 
+            /// and `z2` interchangeably with their references `&z1` and `&z2` in 
+            /// arithmetic expressions involving complex numbers.
+            ///
+            /// Given complex numbers `z1` and `z2`, and their references `&z1` and `&z2`, they 
+            /// should satisfy
+            /// ```text
+            ///  z1 +  z2 = &z1 +  z2
+            ///  z1 +  z2 =  z1 + &z2
+            ///  z1 +  z2 = &z1 + &z2
+            ///  z1 + &z2 = &z1 +  z2
+            /// &z1 +  z2 =  z1 + &z2
+            /// &z1 +  z2 = &z1 + &z2
+            ///  z1 + &z2 = &z1 + &z2
+            /// ```
+            #[test]
+            fn prop_complex1_plus_complex2_equals_refcomplex1_plus_refcomplex2(
+                z1 in $Generator::<$ScalarType>(), z2 in $Generator::<$ScalarType>()) {
+                
+                prop_assert_eq!( z1 +  z2, &z1 +  z2);
+                prop_assert_eq!( z1 +  z2,  z1 + &z2);
+                prop_assert_eq!( z1 +  z2, &z1 + &z2);
+                prop_assert_eq!( z1 + &z2, &z1 +  z2);
+                prop_assert_eq!(&z1 +  z2,  z1 + &z2);
+                prop_assert_eq!(&z1 +  z2, &z1 + &z2);
+                prop_assert_eq!( z1 + &z2, &z1 + &z2);
+            }
+
+            /// Complex number addition over integer scalars should be commutative.
+            ///
+            /// Given complex numbers `z1` and `z2`, we have
+            /// ```text
+            /// z1 + z2 = z2 + z1.
+            /// ```
+            #[test]
+            fn prop_complex_addition_commutative(
+                z1 in $Generator::<$ScalarType>(), z2 in $Generator::<$ScalarType>()) {
+                
+                let zero = Complex::<$ScalarType>::zero();
+
+                prop_assert_eq!((z1 + z2) - (z2 + z1), zero);
+            }
+
+            /// Given three complex numbers of integer scalars, complex number addition 
+            /// should be associative.
+            ///
+            /// Given complex numbers `z1`, `z2`, and `z3`, we have
+            /// ```text
+            /// (z1 + z2) + z3 = z1 + (z2 + z3)
+            /// ```
+            #[test]
+            fn prop_complex_addition_associative(
+                z1 in $Generator::<$ScalarType>(), 
+                z2 in $Generator::<$ScalarType>(), z3 in $Generator::<$ScalarType>()) {
+
+                prop_assert_eq!((z1 + z2) + z3, z1 + (z2 + z3));
+            }
+        }
+    }
+    }
+}
+
+exact_add_props!(complex_i32_add_props, i32, any_complex);
+exact_add_props!(complex_u32_add_props, u32, any_complex);
+
+
+/// Generate property tests for complex number subtraction over floating point scalars.
+///
+/// ### Macro Parameters
+///
+/// The macro parameters are the following:
+/// * `$TestModuleName` is a name we give to the module we place the property 
+///    tests in to separate them from each other for each scalar type to prevent 
+///    namespace collisions.
+/// * `$ScalarType` denotes the underlying system of numbers that compose the 
+///    set of complex numbers.
+/// * `$Generator` is the name of a function or closure for generating examples.
+/// * `$tolerance` specifies the amount of acceptable error for a correct operation 
+///    with floating point scalars.
+macro_rules! approx_sub_props {
+    ($TestModuleName:ident, $ScalarType:ty, $Generator:ident, $tolerance:expr) => {
+    #[cfg(test)]
+    mod $TestModuleName {
+        use proptest::prelude::*;
+        use cglinalg::{
+            Complex,
+        };
+        use super::{
+            $Generator,
+        };
+
+
+        proptest! {
+            /// The zero complex number over floating point scalars should act as an 
+            /// additive unit.
+            ///
+            /// Given a complex number `z`, we have
+            /// ```text
+            /// z - 0 = z
+            /// ```
+            #[test]
+            fn prop_complex_minus_zero_equals_complex(q in $Generator()) {
+                let zero_quat = Complex::<$ScalarType>::zero();
+
+                prop_assert_eq!(q - zero_quat, q);
+            }
+
+            /// Every complex number should have an additive inverse.
+            ///
+            /// Given a complex number `z`, there is a complex number `-z` such that
+            /// ```text
+            /// z - z = z + (-z) = (-z) + z = 0
+            /// ```
+            #[test]
+            fn prop_complex_minus_complex_equals_zero(q in $Generator::<$ScalarType>()) {
+                let zero_quat = Complex::<$ScalarType>::zero();
+
+                prop_assert_eq!(q - q, zero_quat);
+                prop_assert_eq!((-q) + q, zero_quat);
+                prop_assert_eq!(q + (-q), zero_quat);
+            }
+
+            /// Given complex numbers `z1` and `z2`, we should be able to use `z1` and 
+            /// `z2` interchangeably with their references `&z1` and `&z2` in 
+            /// arithmetic expressions involving complex numbers.
+            ///
+            /// Given complex numbers `z1` and `z2`, and their references `&z1` and 
+            /// `&z2`, they should satisfy
+            /// ```text
+            ///  z1 -  z2 = &z1 -  z2
+            ///  z1 -  z2 =  z1 - &z2
+            ///  z1 -  z2 = &z1 - &z2
+            ///  z1 - &z2 = &z1 -  z2
+            /// &z1 -  z2 =  z1 - &z2
+            /// &z1 -  z2 = &z1 - &z2
+            ///  z1 - &z2 = &z1 - &z2
+            /// ```
+            #[test]
+            fn prop_complex1_plus_complex2_equals_refcomplex1_plus_refcomplex2(
+                z1 in $Generator::<$ScalarType>(), z2 in $Generator::<$ScalarType>()) {
+                
+                prop_assert_eq!( z1 -  z2, &z1 -  z2);
+                prop_assert_eq!( z1 -  z2,  z1 - &z2);
+                prop_assert_eq!( z1 -  z2, &z1 - &z2);
+                prop_assert_eq!( z1 - &z2, &z1 -  z2);
+                prop_assert_eq!(&z1 -  z2,  z1 - &z2);
+                prop_assert_eq!(&z1 -  z2, &z1 - &z2);
+                prop_assert_eq!( z1 - &z2, &z1 - &z2);
+            }
+        }
+    }
+    }
+}
+
+approx_sub_props!(complex_f64_sub_props, f64, any_complex, 1e-7);
+
