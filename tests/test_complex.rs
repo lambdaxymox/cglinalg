b0f9a1623a5c6a7ae86e6b4a0283d44487b3b69d
LambdaXymox <lambda.xymox@gmail.com>
2022-03-03 12:53:10 -0600

    Add complex number tests.

--- tests/test_complex.rs
+++ tests/test_complex.rs
@@ -284,3 +284,158 @@ mod arithmetic_tests {
     }
 }
 
+#[cfg(test)]
+mod magnitude_tests {
+    use cglinalg::{
+        Complex,
+    };
+
+
+    #[test]
+    fn test_unit_re_should_have_unit_norm() {
+        let one: Complex<f64> = Complex::unit_re();
+
+        assert_eq!(one.magnitude(), 1_f64);
+    }
+
+    #[test]
+    fn test_unit_im_should_have_unit_norm() {
+        let i: Complex<f64> = Complex::unit_im();
+
+        assert_eq!(i.magnitude(), 1_f64);
+    }
+}
+
+#[cfg(test)]
+mod conjugate_tests {
+    use cglinalg::{
+        Complex,
+    };
+
+
+    #[test]
+    fn test_conjugate() {
+        let z = Complex::new(1_i32, 2_i32);
+        let expected = Complex::new(1_i32, -2_i32);
+        let result = z.conjugate();
+
+        assert_eq!(result, expected);
+    }
+}
+
+#[cfg(test)]
+mod arg_tests {
+    use cglinalg::{
+        Complex,
+    };
+
+
+    #[test]
+    fn test_arg_unit_im() {
+        let i: Complex<f64> = Complex::unit_im();
+        let expected = core::f64::consts::FRAC_PI_2;
+        let result = i.arg();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_arg_unit_re() {
+        let one: Complex<f64> = Complex::unit_re();
+        let expected = 0_f64;
+        let result = one.arg();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_arg_minus_unit_im() {
+        let minus_i: Complex<f64> = -Complex::unit_im();
+        let expected = -core::f64::consts::FRAC_PI_2;
+        let result = minus_i.arg();
+    }
+
+    #[test]
+    fn test_arg_minus_unit_re() {
+        let minus_one: Complex<f64> = -Complex::unit_re();
+        let expected = -core::f64::consts::PI;
+        let result = minus_one.arg();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_arg_complex1() {
+        let z = Complex::new(1_f64, 1_f64);
+        let expected = core::f64::consts::FRAC_PI_4;
+        let result = z.arg();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_arg_complex2() {
+        let z = Complex::new(1_f64, -1_f64);
+        let expected = -core::f64::consts::FRAC_PI_4;
+        let result = z.arg();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_arg_complex3() {
+        let z = Complex::new(-1_f64, 1_f64);
+        let expected = 3_f64 * core::f64::consts::FRAC_PI_4;
+        let result = z.arg();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_arg_complex4() {
+        let z = Complex::new(-1_f64, -1_f64);
+        let expected = -3_f64 * core::f64::consts::FRAC_PI_4;
+        let result = z.arg();
+
+        assert_eq!(result, expected);
+    }
+}
+/*
+#[cfg(test)]
+mod inverse_tests {
+    use cglinalg::{
+        Complex,
+    };
+
+
+    #[test]
+    fn test_inverse_zero() {
+        let zero: Complex<f64> = Complex::zero();
+
+        assert!(zero.inverse().is_none());
+    }
+
+    #[test]
+    fn test_inverse_unit_re() {
+        let one: Complex<f64> = Complex::unit_re();
+
+        assert_eq!(one.inverse(), one);
+    }
+
+    #[test]
+    fn test_inverse_unit_im() {
+        let i: Complex<f64> = Complex::unit_im();
+
+        assert_eq!(i.inverse().unwrap(), -i);
+    }
+
+    #[test]
+    fn test_inverse_real() {
+        let z = Complex::from_real(2_f64);
+        let expected = Complex::from_real(1_f64 / 2_f64);
+        let result = z.inverse().unwrap();
+
+        assert_eq!(result, expected);
+    }
+}
+*/
