8cd7f5e9bfd51e860083c59ce10bfe772a148597
LambdaXymox <lambda.xymox@gmail.com>
2022-03-06 19:16:52 -0600

    Add benchmarks for dot and cross products.

--- benches/bench_vector.rs
+++ benches/bench_vector.rs
@@ -84,8 +84,8 @@ where
 }
 
 
-macro_rules! benchmark_binary_op(
-    ($name: ident, $generator_t1:ident, $generator_t2:ident, $scalar_type:ty, $type1:ty, $type2:ty, $binop:ident) => {
+macro_rules! bench_binop(
+    ($name: ident, $scalar_type:ty, $type1:ty, $type2:ty, $generator_t1:ident, $generator_t2:ident, $binop:ident) => {
         fn $name(bh: &mut criterion::Criterion) {
             let a = $generator_t1::<$scalar_type>();
             let b = $generator_t2::<$scalar_type>();
@@ -97,30 +97,63 @@ macro_rules! benchmark_binary_op(
     }
 );
 
-benchmark_binary_op!(vector1_add_vector1, gen_vector1, gen_vector1, f32, Vector1<f32>, Vector1<f32>, add);
-benchmark_binary_op!(vector2_add_vector2, gen_vector2, gen_vector2, f32, Vector2<f32>, Vector2<f32>, add);
-benchmark_binary_op!(vector3_add_vector3, gen_vector3, gen_vector3, f32, Vector3<f32>, Vector3<f32>, add);
-benchmark_binary_op!(vector4_add_vector4, gen_vector4, gen_vector4, f32, Vector4<f32>, Vector4<f32>, add);
+macro_rules! bench_binop_ref(
+    ($name: ident, $scalar_type:ty, $type1:ty, $type2:ty, $generator_t1:ident, $generator_t2:ident, $binop:ident) => {
+        fn $name(bh: &mut criterion::Criterion) {
+            let a = $generator_t1::<$scalar_type>();
+            let b = $generator_t2::<$scalar_type>();
 
-benchmark_binary_op!(vector1_sub_vector1, gen_vector1, gen_vector1, f32, Vector1<f32>, Vector1<f32>, sub);
-benchmark_binary_op!(vector2_sub_vector2, gen_vector2, gen_vector2, f32, Vector2<f32>, Vector2<f32>, sub);
-benchmark_binary_op!(vector3_sub_vector3, gen_vector3, gen_vector3, f32, Vector3<f32>, Vector3<f32>, sub);
-benchmark_binary_op!(vector4_sub_vector4, gen_vector4, gen_vector4, f32, Vector4<f32>, Vector4<f32>, sub);
+            bh.bench_function(stringify!($name), move |bh| bh.iter(|| {
+                a.$binop(&b)
+            }));
+        }
+    }
+);
 
-benchmark_binary_op!(scalar_mul_vector1,  gen_scalar,  gen_vector1, f32, f32,          Vector1<f32>, mul);
-benchmark_binary_op!(scalar_mul_vector2,  gen_scalar,  gen_vector2, f32, f32,          Vector2<f32>, mul);
-benchmark_binary_op!(scalar_mul_vector3,  gen_scalar,  gen_vector3, f32, f32,          Vector3<f32>, mul);
-benchmark_binary_op!(scalar_mul_vector4,  gen_scalar,  gen_vector4, f32, f32,          Vector4<f32>, mul);
+macro_rules! bench_biop_fn(
+    ($name: ident, $scalar_type:ty, $type1:ty, $type2:ty, $generator_t1:ident, $generator_t2:ident, $binop:ident) => {
+        fn $name(bh: &mut criterion::Criterion) {
+            let a = $generator_t1::<$scalar_type>();
+            let b = $generator_t2::<$scalar_type>();
 
-benchmark_binary_op!(vector1_mul_scalar,  gen_vector1, gen_scalar,  f32, Vector1<f32>, f32,          mul);
-benchmark_binary_op!(vector2_mul_scalar,  gen_vector2, gen_scalar,  f32, Vector2<f32>, f32,          mul);
-benchmark_binary_op!(vector3_mul_scalar,  gen_vector3, gen_scalar,  f32, Vector3<f32>, f32,          mul);
-benchmark_binary_op!(vector4_mul_scalar,  gen_vector4, gen_scalar,  f32, Vector4<f32>, f32,          mul);
+            bh.bench_function(stringify!($name), move |bh| bh.iter(|| {
+                $binop(&a, &b)
+            }));
+        }
+    }
+);
 
-benchmark_binary_op!(vector1_div_scalar,  gen_vector1, gen_scalar,  f32, Vector1<f32>, f32,          div);
-benchmark_binary_op!(vector2_div_scalar,  gen_vector2, gen_scalar,  f32, Vector2<f32>, f32,          div);
-benchmark_binary_op!(vector3_div_scalar,  gen_vector3, gen_scalar,  f32, Vector3<f32>, f32,          div);
-benchmark_binary_op!(vector4_div_scalar,  gen_vector4, gen_scalar,  f32, Vector4<f32>, f32,          div);
+bench_binop!(vector1_add_vector1, f32, Vector1<f32>, Vector1<f32>, gen_vector1, gen_vector1, add);
+bench_binop!(vector2_add_vector2, f32, Vector2<f32>, Vector2<f32>, gen_vector2, gen_vector2, add);
+bench_binop!(vector3_add_vector3, f32, Vector3<f32>, Vector3<f32>, gen_vector3, gen_vector3, add);
+bench_binop!(vector4_add_vector4, f32, Vector4<f32>, Vector4<f32>, gen_vector4, gen_vector4, add);
+
+bench_binop!(vector1_sub_vector1, f32, Vector1<f32>, Vector1<f32>, gen_vector1, gen_vector1, sub);
+bench_binop!(vector2_sub_vector2, f32, Vector2<f32>, Vector2<f32>, gen_vector2, gen_vector2, sub);
+bench_binop!(vector3_sub_vector3, f32, Vector3<f32>, Vector3<f32>, gen_vector3, gen_vector3, sub);
+bench_binop!(vector4_sub_vector4, f32, Vector4<f32>, Vector4<f32>, gen_vector4, gen_vector4, sub);
+
+bench_binop!(scalar_mul_vector1,  f32, f32,          Vector1<f32>, gen_scalar,  gen_vector1, mul);
+bench_binop!(scalar_mul_vector2,  f32, f32,          Vector2<f32>, gen_scalar,  gen_vector2, mul);
+bench_binop!(scalar_mul_vector3,  f32, f32,          Vector3<f32>, gen_scalar,  gen_vector3, mul);
+bench_binop!(scalar_mul_vector4,  f32, f32,          Vector4<f32>, gen_scalar,  gen_vector4, mul);
+
+bench_binop!(vector1_mul_scalar,  f32, Vector1<f32>, f32,          gen_vector1, gen_scalar,  mul);
+bench_binop!(vector2_mul_scalar,  f32, Vector2<f32>, f32,          gen_vector2, gen_scalar,  mul);
+bench_binop!(vector3_mul_scalar,  f32, Vector3<f32>, f32,          gen_vector3, gen_scalar,  mul);
+bench_binop!(vector4_mul_scalar,  f32, Vector4<f32>, f32,          gen_vector4, gen_scalar,  mul);
+
+bench_binop!(vector1_div_scalar,  f32, Vector1<f32>, f32,          gen_vector1, gen_scalar,  div);
+bench_binop!(vector2_div_scalar,  f32, Vector2<f32>, f32,          gen_vector2, gen_scalar,  div);
+bench_binop!(vector3_div_scalar,  f32, Vector3<f32>, f32,          gen_vector3, gen_scalar,  div);
+bench_binop!(vector4_div_scalar,  f32, Vector4<f32>, f32,          gen_vector4, gen_scalar,  div);
+
+bench_binop_ref!(vector1_dot_vector1, f32, Vector1<f32>, Vector1<f32>, gen_vector1, gen_vector1, dot);
+bench_binop_ref!(vector2_dot_vector2, f32, Vector2<f32>, Vector2<f32>, gen_vector2, gen_vector2, dot);
+bench_binop_ref!(vector3_dot_vector3, f32, Vector3<f32>, Vector3<f32>, gen_vector3, gen_vector3, dot);
+bench_binop_ref!(vector4_dot_vector4, f32, Vector4<f32>, Vector4<f32>, gen_vector4, gen_vector4, dot);
+
+bench_binop_ref!(vector3_cross_vector3, f32, Vector3<f32>, Vector3<f32>, gen_vector3, gen_vector3, cross);
 
 criterion_group!(
     vector_benches, 
@@ -144,6 +177,11 @@ criterion_group!(
     vector2_div_scalar,
     vector3_div_scalar,
     vector4_div_scalar,
+    vector1_dot_vector1,
+    vector2_dot_vector2,
+    vector3_dot_vector3,
+    vector4_dot_vector4,
+    vector3_cross_vector3,
 );
 criterion_main!(vector_benches);
 
