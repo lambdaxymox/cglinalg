af0c38e0a07abb7b485333771aa9cb9f9d4cf321
LambdaXymox <lambda.xymox@gmail.com>
2022-09-09 16:44:34 -0500

    Minor refactoring.

--- src/isometry.rs
+++ src/isometry.rs
@@ -247,7 +247,7 @@ where
         Matrix3x3::new(
             rotation_matrix.c0r0, rotation_matrix.c0r1, zero,
             rotation_matrix.c1r0, rotation_matrix.c1r1, zero,
-            translation[0],       translation[1],  one
+            translation[0],       translation[1],       one
         )
     }
     
@@ -1133,7 +1133,7 @@ where
             rotation_matrix.c0r0, rotation_matrix.c0r1, rotation_matrix.c0r2, zero,
             rotation_matrix.c1r0, rotation_matrix.c1r1, rotation_matrix.c1r2, zero,
             rotation_matrix.c2r0, rotation_matrix.c2r1, rotation_matrix.c2r2, zero,
-            translation[0], translation[1], translation[2], one
+            translation[0], translation[1], translation[2],  one
         )
     }
     
