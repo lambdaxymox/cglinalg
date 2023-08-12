69a991ee123595979bec819332990bad062bbff2
LambdaXymox <lambda.xymox@gmail.com>
2023-08-12 11:34:06 -0500

    Add more tests and a property specification for the Unit type.

---
+++ crates/cglinalg_core/tests/spec_unit.rs
@@ -0,0 +1,195 @@
+extern crate cglinalg_core;
+extern crate num_traits;
+extern crate proptest;
+
+
+use cglinalg_core::{
+    Complex,
+    Quaternion,
+    Vector1, 
+    Vector2, 
+    Vector3, 
+    Vector4,
+    SimdScalar,
+};
+
+use proptest::prelude::*;
+
+
+fn any_vector1<S>() -> impl Strategy<Value = Vector1<S>> 
+where 
+    S: SimdScalar + Arbitrary 
+{
+    any::<S>().prop_map(|x| {
+        let eps = num_traits::cast::<f64, S>(1e-6_f64).unwrap();
+        let vector_eps = Vector1::from_fill(eps);
+        let modulus = num_traits::cast(100_000_000).unwrap();
+        let vector = Vector1::new(x);
+
+        vector_eps + vector % modulus
+    })
+}
+
+fn any_vector2<S>() -> impl Strategy<Value = Vector2<S>> 
+where 
+    S: SimdScalar + Arbitrary
+{
+    any::<(S, S)>().prop_map(|(x, y)| {
+        let eps = num_traits::cast::<f64, S>(1e-6_f64).unwrap();
+        let vector_eps = Vector2::from_fill(eps);
+        let modulus = num_traits::cast(100_000_000).unwrap();
+        let vector = Vector2::new(x, y);
+
+        vector_eps + vector % modulus
+    })
+}
+
+fn any_vector3<S>() -> impl Strategy<Value = Vector3<S>>
+where 
+    S: SimdScalar + Arbitrary
+{
+    any::<(S, S, S)>().prop_map(|(x, y, z)| {
+        let eps = num_traits::cast::<f64, S>(1e-6_f64).unwrap();
+        let vector_eps = Vector3::from_fill(eps);
+        let modulus = num_traits::cast(100_000_000).unwrap();
+        let vector = Vector3::new(x, y, z);
+
+        vector_eps + vector % modulus
+    })
+}
+
+fn any_vector4<S>() -> impl Strategy<Value = Vector4<S>>
+where 
+    S: SimdScalar + Arbitrary
+{
+    any::<(S, S, S, S)>().prop_map(|(x, y, z, w)| {
+        let eps = num_traits::cast::<f64, S>(1e-6_f64).unwrap();
+        let vector_eps = Vector4::from_fill(eps);
+        let modulus = num_traits::cast(100_000_000).unwrap();
+        let vector = Vector4::new(x, y, z, w);
+
+        vector_eps + vector % modulus
+    })
+}
+
+fn any_quaternion<S>() -> impl Strategy<Value = Quaternion<S>> 
+where 
+    S: SimdScalar + Arbitrary
+{
+    any::<(S, S, S, S)>().prop_map(|(x, y, z, w)| {
+        let eps = num_traits::cast::<f64, S>(1e-6_f64).unwrap();
+        let quaternion_eps = Quaternion::from_fill(eps);
+        let modulus: S = num_traits::cast(100_000_000).unwrap();
+        let quaternion = Quaternion::new(x, y, z, w);
+
+        quaternion_eps + quaternion % modulus
+    })
+    .no_shrink()
+}
+
+fn any_complex<S>() -> impl Strategy<Value = Complex<S>> 
+where 
+    S: SimdScalar + Arbitrary
+{
+    any::<(S, S)>().prop_map(|(x, y)| {
+        let eps = num_traits::cast::<f64, S>(1e-6_f64).unwrap();
+        let complex_eps = Complex::new(eps, eps);
+        let modulus: S = num_traits::cast(100_000_000).unwrap();
+        let complex = Complex::new(x, y);
+
+        complex_eps + complex % modulus
+    })
+    .no_shrink()
+}
+
+
+/// Generate property tests for normed data types.
+///
+/// ### Macro Parameters
+///
+/// The macro parameters are the following:
+/// * `$TestModuleName` is a name we give to the module we place the property tests 
+///    in to separate them from each other for each scalar type to prevent 
+///    namespace collisions.
+/// * `$VectorN` denotes the name of the normed type.
+/// * `$ScalarType` denotes the underlying system of numbers that compose the normed type.
+/// * `$Generator` is the name of a function or closure for generating examples.
+macro_rules! unit_props {
+    ($TestModuleName:ident, $UnitType:ident, $ScalarType:ty, $Generator:ident) => {
+    #[cfg(test)]
+    mod $TestModuleName {
+        use proptest::prelude::*;
+        use cglinalg_core::{
+            Normed,
+            Unit,
+        };
+        use super::{
+            $Generator,
+        };
+
+
+        proptest! {
+            #[test]
+            fn prop_from_value_normalized(value in $Generator::<$ScalarType>()) {
+                let expected = value.normalize();
+                let unit_value = Unit::from_value(value);
+                let result = unit_value.into_inner();
+
+                prop_assert_eq!(result, expected);
+            }
+
+            #[test]
+            fn prop_from_value_with_norm_normalized(value in $Generator::<$ScalarType>()) {
+                let expected = value.normalize();
+                let (unit_value, _) = Unit::from_value_with_norm(value);
+                let result = unit_value.into_inner();
+
+                prop_assert_eq!(result, expected);
+            }
+
+            #[test]
+            fn prop_from_value_with_norm_correct_norm(value in $Generator::<$ScalarType>()) {
+                let expected = value.norm();
+                let (_, result) = Unit::from_value_with_norm(value);
+
+                prop_assert_eq!(result, expected);
+            }
+
+            #[test]
+            fn prop_from_value_unchecked_into_inner(value in $Generator::<$ScalarType>()) {
+                let expected = value;
+                let unit_value = Unit::from_value_unchecked(value);
+                let result = unit_value.into_inner();
+
+                prop_assert_eq!(result, expected);
+            }
+
+            #[test]
+            fn prop_try_from_value_with_norm_above_threshold_is_some(value in $Generator::<$ScalarType>()) {
+                let threshold = num_traits::cast::<f64, $ScalarType>(1e-8).unwrap();
+                let result = Unit::try_from_value_with_norm(value, threshold);
+
+                prop_assert!(result.is_some());
+            }
+
+            #[test]
+            fn prop_try_from_value_above_threshold_is_some(value in $Generator::<$ScalarType>()) {
+                let threshold = num_traits::cast::<f64, $ScalarType>(1e-8).unwrap();
+                let result = Unit::try_from_value(value, threshold);
+
+                prop_assert!(result.is_some());
+            }
+        }
+    }
+    }
+}
+
+unit_props!(unit_vector1_f64_props, Vector1, f64, any_vector1);
+unit_props!(unit_vector2_f64_props, Vector2, f64, any_vector2);
+unit_props!(unit_vector3_f64_props, Vector3, f64, any_vector3);
+unit_props!(unit_vector4_f64_props, Vector4, f64, any_vector4);
+
+unit_props!(unit_quaternion_f64_props, Quaternion, f64, any_quaternion);
+
+unit_props!(unit_complex_f64_props, Complex, f64, any_complex);
+
--- crates/cglinalg_core/tests/test_unit.rs
+++ crates/cglinalg_core/tests/test_unit.rs
@@ -3,52 +3,206 @@ extern crate cglinalg_core;
 
 use cglinalg_core::{
     Unit,
+    Vector1,
+    Vector2,
     Vector3,
+    Vector4,
+};
+use approx::{
+    assert_relative_eq,
 };
 
 
-/// Pass a unit vector into a `Unit` constructor should not affect the unit vector.
-/// A unit vector should have a unit norm.
 #[test]
-fn test_vector3_unit_vectors() {
-    let unit_x: Vector3<f64> = Vector3::unit_x();
-    let unit_y: Vector3<f64> = Vector3::unit_y();
-    let unit_z: Vector3<f64> = Vector3::unit_z();
-
+fn test_unit_vector1_unit_x() {
+    let unit_x: Vector2<f64> = Vector2::unit_x();
     let (unit_unit_x, norm_unit_x) = Unit::from_value_with_norm(unit_x); 
-    let (unit_unit_y, norm_unit_y) = Unit::from_value_with_norm(unit_y);
-    let (unit_unit_z, norm_unit_z) = Unit::from_value_with_norm(unit_z);
 
     assert_eq!(norm_unit_x, 1_f64);
-    assert_eq!(norm_unit_y, 1_f64);
-    assert_eq!(norm_unit_z, 1_f64);
-
     assert_eq!(unit_unit_x.as_ref(), &unit_x);
+}
+
+#[test]
+fn test_unit_vector2_unit_x() {
+    let unit_x: Vector2<f64> = Vector2::unit_x();
+    let (unit_unit_x, norm_unit_x) = Unit::from_value_with_norm(unit_x); 
+    
+    assert_eq!(norm_unit_x, 1_f64);
+    assert_eq!(unit_unit_x.as_ref(), &unit_x);
+}
+
+#[test]
+fn test_unit_vector2_unit_y() {
+    let unit_y: Vector2<f64> = Vector2::unit_y();
+    let (unit_unit_y, norm_unit_y) = Unit::from_value_with_norm(unit_y);
+    
+    assert_eq!(norm_unit_y, 1_f64);
     assert_eq!(unit_unit_y.as_ref(), &unit_y);
+}
+
+#[test]
+fn test_unit_vector3_unit_x() {
+    let unit_x: Vector3<f64> = Vector3::unit_x();
+    let (unit_unit_x, norm_unit_x) = Unit::from_value_with_norm(unit_x); 
+    
+    assert_eq!(norm_unit_x, 1_f64);
+    assert_eq!(unit_unit_x.as_ref(), &unit_x);
+}
+
+#[test]
+fn test_unit_vector3_unit_y() {
+    let unit_y: Vector3<f64> = Vector3::unit_y();
+    let (unit_unit_y, norm_unit_y) = Unit::from_value_with_norm(unit_y);
+
+    assert_eq!(norm_unit_y, 1_f64);
+    assert_eq!(unit_unit_y.as_ref(), &unit_y);
+}
+
+#[test]
+fn test_unit_vector3_unit_z() {
+    let unit_z: Vector3<f64> = Vector3::unit_z();
+    let (unit_unit_z, norm_unit_z) = Unit::from_value_with_norm(unit_z);
+    
+    assert_eq!(norm_unit_z, 1_f64);
     assert_eq!(unit_unit_z.as_ref(), &unit_z);
 }
 
-/// A `Unit` constructor should correctly normalize the resulting vector.
 #[test]
-fn test_vector() {
-    let vector = Vector3::new(2.0, 2.0, 2.0);
+fn test_unit_vector4_unit_x() {
+    let unit_x: Vector4<f64> = Vector4::unit_x();
+    let (unit_unit_x, norm_unit_x) = Unit::from_value_with_norm(unit_x); 
+    
+    assert_eq!(norm_unit_x, 1_f64);
+    assert_eq!(unit_unit_x.as_ref(), &unit_x);    
+}
+
+#[test]
+fn test_unit_vector4_unit_y() {
+    let unit_y: Vector4<f64> = Vector4::unit_y();
+    let (unit_unit_y, norm_unit_y) = Unit::from_value_with_norm(unit_y);
+
+    assert_eq!(norm_unit_y, 1_f64);
+    assert_eq!(unit_unit_y.as_ref(), &unit_y);
+}
+
+#[test]
+fn test_unit_vector4_unit_z() {
+    let unit_z: Vector4<f64> = Vector4::unit_z();
+    let (unit_unit_z, norm_unit_z) = Unit::from_value_with_norm(unit_z);
+
+    assert_eq!(norm_unit_z, 1_f64);
+    assert_eq!(unit_unit_z.as_ref(), &unit_z);
+}
+
+#[test]
+fn test_unit_vector4_unit_w() {
+    let unit_w: Vector4<f64> = Vector4::unit_w();
+    let (unit_unit_w, norm_unit_w) = Unit::from_value_with_norm(unit_w);
+
+    assert_eq!(norm_unit_w, 1_f64);
+    assert_eq!(unit_unit_w.as_ref(), &unit_w);
+}
+
+#[test]
+fn test_unit_vector1() {
+    let vector = Vector1::new(3_f64);
     let (wrapped, norm) = Unit::from_value_with_norm(vector);
     let unit_vector = wrapped.as_ref();
 
-    assert_eq!(unit_vector.norm(), 1.0);
+    assert_relative_eq!(unit_vector.norm(), 1.0, epsilon = 1e-10);
     assert_eq!(vector.norm(), norm);
 }
 
-
-/// The `try_new` function should return `None` when the
-/// vector is smaller than the threshold.
 #[test]
-fn test_vector_close_to_zero() {
+fn test_unit_vector2() {
+    let vector = Vector2::new(3_f64, 4_f64);
+    let (wrapped, norm) = Unit::from_value_with_norm(vector);
+    let unit_vector = wrapped.as_ref();
+
+    assert_relative_eq!(unit_vector.norm(), 1.0, epsilon = 1e-10);
+    assert_eq!(vector.norm(), norm);
+}
+
+#[test]
+fn test_unit_vector3() {
+    let vector = Vector3::new(3_f64, 4_f64, 5_f64);
+    let (wrapped, norm) = Unit::from_value_with_norm(vector);
+    let unit_vector = wrapped.as_ref();
+
+    assert_relative_eq!(unit_vector.norm(), 1.0, epsilon = 1e-10);
+    assert_eq!(vector.norm(), norm);
+}
+
+#[test]
+fn test_unit_vector4() {
+    let vector = Vector4::new(3_f64, 4_f64, 5_f64, 6_f64);
+    let (wrapped, norm) = Unit::from_value_with_norm(vector);
+    let unit_vector = wrapped.as_ref();
+
+    assert_relative_eq!(unit_vector.norm(), 1.0, epsilon = 1e-10);
+    assert_eq!(vector.norm(), norm);
+}
+
+#[test]
+fn test_unit_vector1_close_to_zero_returns_none() {
     let threshold = 3_f64 * f64::EPSILON;
-    let vector = Vector3::new(f64::EPSILON, 0.0, 0.0);
-    let expected = None;
+    let vector = Vector1::new(f64::EPSILON);
     let result = Unit::try_from_value(vector, threshold);
 
-    assert_eq!(result, expected);
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_unit_vector2_close_to_zero_returns_none1() {
+    let threshold = 3_f64 * f64::EPSILON;
+    let vector = Vector2::new(f64::EPSILON, 0_f64);
+    let result = Unit::try_from_value(vector, threshold);
+
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_unit_vector2_close_to_zero_returns_none2() {
+    let threshold = 3_f64 * f64::EPSILON;
+    let vector = Vector2::new(0_f64, f64::EPSILON);
+    let result = Unit::try_from_value(vector, threshold);
+
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_unit_vector3_close_to_zero_returns_none1() {
+    let threshold = 3_f64 * f64::EPSILON;
+    let vector = Vector3::new(f64::EPSILON, 0_f64, 0_f64);
+    let result = Unit::try_from_value(vector, threshold);
+
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_unit_vector3_close_to_zero_returns_none2() {
+    let threshold = 3_f64 * f64::EPSILON;
+    let vector = Vector3::new(0_f64, f64::EPSILON, 0_f64);
+    let result = Unit::try_from_value(vector, threshold);
+
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_unit_vector3_close_to_zero_returns_none3() {
+    let threshold = 3_f64 * f64::EPSILON;
+    let vector = Vector3::new(0_f64, 0_f64, f64::EPSILON);
+    let result = Unit::try_from_value(vector, threshold);
+
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_unit_vector4_close_to_zero_returns_none() {
+    let threshold = 3_f64 * f64::EPSILON;
+    let vector = Vector4::new(f64::EPSILON, 0_f64, 0_f64, 0_f64);
+    let result = Unit::try_from_value(vector, threshold);
+
+    assert!(result.is_none());
 }
 
